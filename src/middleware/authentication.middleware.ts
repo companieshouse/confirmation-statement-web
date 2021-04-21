d9d38b90a2c53a3e17b25204826a873a18aa6c92
Alfred Bwomezi <ABwomezi@companieshouse.gov.uk>
2021-04-21 12:13:57 +0100

    NCS-46 formatting lint fixes.

--- src/middleware/authentication.middleware.ts
+++ src/middleware/authentication.middleware.ts
@@ -2,11 +2,11 @@ import { NextFunction, Request, Response } from "express";
 import { authMiddleware, AuthOptions } from "@companieshouse/web-security-node";
 
 export const authenticationMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    
-    const authMiddlewareConfig: AuthOptions = {
-        accountWebUrl: "",
-        returnUrl: req.originalUrl
-    };
 
-    return authMiddleware(authMiddlewareConfig)(req, res, next);
+  const authMiddlewareConfig: AuthOptions = {
+    accountWebUrl: "",
+    returnUrl: req.originalUrl
+  };
+
+  return authMiddleware(authMiddlewareConfig)(req, res, next);
 };
