ee1db1ff6aa056013c6e715ab4c5be8bf916968c
ICoxCH <ICox@companieshouse.gov.uk>
2021-04-21 15:11:26 +0100

    mocked auth middleware for unit tests

--- test/controllers/start.controller.unit.ts
+++ test/controllers/start.controller.unit.ts
@@ -1,6 +1,17 @@
+import { NextFunction, Request, Response } from "express";
 import chai from "chai";
 import chaiHttp from "chai-http";
+import sinon from "sinon";
+import decache from "decache";
+decache('../../src/app');
 chai.use(chaiHttp);
+const auth = require ("../../src/middleware/authentication.middleware");
+
+// store reference to original function in case you need it:
+const originalAuthMid = auth.authenticationMiddleware
+
+// replace isAdmin method with a stubbed, but don't specify implementation yet
+const authMidStub = sinon.stub(auth, "authenticationMiddleware");
 
 import app from "../../src/app";
 
@@ -8,6 +19,7 @@ const expect = chai.expect;
 
 describe("start controller tests", function() {
   it("should return start page", async function() {
+    auth.authenticationMiddleware.callsFake((req : Request, res: Response, next: NextFunction) => next());
     const response = await chai.request(app).get("/confirmation-statement/");
 
     expect(response.text).to.contain("File a confirmation statement");
