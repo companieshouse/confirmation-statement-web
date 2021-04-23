8ccdc0101f99725b72be40715f1f618a098e8677
kheseyongya <kheseyongya@gmail.com>
2021-04-23 14:51:25 +0100

    Add company number unit test

---
+++ test/controllers/company.number.controller.unit.ts
@@ -0,0 +1,36 @@
+jest.mock("ioredis");
+jest.mock("../../src/middleware/service.availability.middleware");
+jest.mock("../../src/middleware/authentication.middleware");
+jest.mock("../../src/middleware/session.middleware");
+
+import { NextFunction, Request, Response } from "express";
+import request from "supertest";
+import app from "../../src/app";
+import { serviceAvailabilityMiddleware } from "../../src/middleware/service.availability.middleware";
+import { authenticationMiddleware } from "../../src/middleware/authentication.middleware";
+import { sessionMiddleware } from "../../src/middleware/session.middleware";
+
+// create a dummy middleware function to use in the mocks
+const dummyMiddleware = (req: Request, res: Response, next: NextFunction) => next();
+
+// get handles on mocked functions
+const mockServiceAvailabilityMiddleware = serviceAvailabilityMiddleware as jest.Mock;
+const mockAuthenticationMiddleware = authenticationMiddleware as jest.Mock;
+const mockSessionMiddleware = sessionMiddleware as jest.Mock;
+
+// tell the mocks what to return
+mockServiceAvailabilityMiddleware.mockImplementation(dummyMiddleware);
+mockAuthenticationMiddleware.mockImplementation(dummyMiddleware);
+mockSessionMiddleware.mockImplementation(dummyMiddleware);
+
+const EXPECTED_TEXT = "This is the company number page";
+
+describe("company number controller tests", () => {
+  it("should return company number page", async () => {
+    const response = await request(app)
+      .get("/confirmation-statement/company-number");
+
+    expect(response.text).toContain(EXPECTED_TEXT);
+    expect(mockAuthenticationMiddleware).toHaveBeenCalled;
+  });
+});
