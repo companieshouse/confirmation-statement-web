43b1f4d832fd41b0d2871a3be15a85248214007d
Jack Skinner <jskinner@companieshouse.gov.uk>
2021-04-30 13:10:03 +0100

    ncs-90 adding service for getting company profile

--- src/controllers/confirm.company.controller.ts
+++ src/controllers/confirm.company.controller.ts
@@ -1,6 +1,12 @@
 import { Request, Response } from "express";
 import { Templates } from "../types/template.paths";
+import logger from "../utils/logger";
 
-export const get = (req: Request, res: Response) => {
+import { getCompanyProfile } from "../services/company.profile.service";
+import { CompanyProfile } from "@companieshouse/api-sdk-node/dist/services/company-profile/types";
+
+export const get = async (req: Request, res: Response) => {
+  const companyProfile: CompanyProfile = await getCompanyProfile(req.query.companyNumber as string);
+  logger.info(JSON.stringify(companyProfile, null, 2));
   return res.render(Templates.CONFIRM_COMPANY);
 };
---
+++ src/services/company.profile.service.ts
@@ -0,0 +1,25 @@
+import { createApiClient, Resource } from "@companieshouse/api-sdk-node";
+import { CompanyProfile } from "@companieshouse/api-sdk-node/dist/services/company-profile/types";
+import { CHS_API_KEY } from "../utils/properties";
+import logger from "../utils/logger";
+
+export const getCompanyProfile = async (companyNumber: string): Promise<CompanyProfile> => {
+  const apiClient = createApiClient(CHS_API_KEY);
+
+  logger.debug(`Looking for company profile with company number ${companyNumber}`);
+  const sdkResponse: Resource<CompanyProfile> = await apiClient.companyProfile.getCompanyProfile(companyNumber);
+
+  if (sdkResponse.httpStatusCode >= 400) {
+    const error = new Error ("Failed to get company profile");
+    logger.error(`${error.message} - ${error.stack}`);
+    throw error;
+  }
+
+  if (sdkResponse.resource) {
+    return sdkResponse.resource;
+  } else {
+    const error = new Error ("Company profile is undefined");
+    logger.error(`${error.message} - ${error.stack}`);
+    throw error;
+  }
+};
