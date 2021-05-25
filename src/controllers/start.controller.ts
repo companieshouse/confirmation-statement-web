d17b9a1564d317a54916896bdd1565b7c409d2b9
Jack Skinner <jskinner@companieshouse.gov.uk>
2021-05-25 16:12:06 +0100

    add env variable for piwik start goal id

--- src/controllers/start.controller.ts
+++ src/controllers/start.controller.ts
@@ -2,5 +2,5 @@ import { Request, Response } from "express";
 import { Templates } from "../types/template.paths";
 
 export const get = (req: Request, res: Response) => {
-  return res.render(Templates.START, { CHS_URL: process.env.CHS_URL, templateName: Templates.START });
+  return res.render(Templates.START, { CHS_URL: process.env.CHS_URL, START_GOAL_ID: process.env.PIWIK_START_GOAL_ID, templateName: Templates.START });
 };
