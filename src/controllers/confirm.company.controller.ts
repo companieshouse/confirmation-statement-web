fc0a4074c661fa48ff8b593eef42d4843d7be4b4
Matthew Culpin <mculpin@companieshouse.gov.uk>
2021-05-26 15:01:58 +0100

    NCS-158 await required

--- src/controllers/confirm.company.controller.ts
+++ src/controllers/confirm.company.controller.ts
@@ -26,7 +26,7 @@ export const post = async (req: Request, res: Response) => {
   const session: Session = req.session as Session;
   const eligibilityStatusCode: EligibilityStatusCode = await checkEligibility(session, company.companyNumber);
   if (eligibilityStatusCode === EligibilityStatusCode.COMPANY_VALID_FOR_SERVICE) {
-    createNewConfirmationStatement(req);
+    await createNewConfirmationStatement(req);
     const url = TRADING_STATUS_PATH.replace(":companyNumber", company.companyNumber);
     return res.redirect(url);
   } else {
