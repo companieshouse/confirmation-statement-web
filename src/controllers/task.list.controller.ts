696b8e6285fc04d794ae7fd3fa80e56ca3d670a1
Matthew Culpin <mculpin@companieshouse.gov.uk>
2021-06-04 10:56:43 +0100

    NCS-368 and 9 use urls

--- src/controllers/task.list.controller.ts
+++ src/controllers/task.list.controller.ts
@@ -3,11 +3,11 @@ import { Templates } from "../types/template.paths";
 import { initTaskList, TaskList } from "../types/task.list";
 import { CompanyProfile } from "@companieshouse/api-sdk-node/dist/services/company-profile/types";
 import { getCompanyProfile } from "../services/company.profile.service";
-import { urlParams } from "../types/page.urls";
+import { TRADING_STATUS_PATH, urlParams } from "../types/page.urls";
 
 export const get = async (req: Request, res: Response) => {
   const companyNumber = req.params[urlParams.PARAM_COMPANY_NUMBER];
-  const backLinkUrl = `/confirmation-statement/company/${companyNumber}/trading-status`;
+  const backLinkUrl = TRADING_STATUS_PATH.replace(`:${urlParams.PARAM_COMPANY_NUMBER}`, companyNumber);
   const company: CompanyProfile = await getCompanyProfile(companyNumber);
   const taskList: TaskList = initTaskList();
   return res.render(Templates.TASK_LIST, {
--- src/controllers/trading.status.controller.ts
+++ src/controllers/trading.status.controller.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from "express";
 import { Templates } from "../types/template.paths";
-import { TASK_LIST_PATH, urlParams } from "../types/page.urls";
+import { CONFIRM_COMPANY_PATH, TASK_LIST_PATH, TRADING_STATUS_PATH, urlParams } from "../types/page.urls";
 import { TRADING_STATUS_ERROR } from "../utils/constants";
 
 export const get = (req: Request, res: Response) => {
   const companyNumber =  req.params[urlParams.PARAM_COMPANY_NUMBER];
-  const backLinkUrl = `/confirmation-statement/confirm-company?companyNumber=${companyNumber}`;
+  const backLinkUrl = `${CONFIRM_COMPANY_PATH}?companyNumber=${companyNumber}`;
   return res.render(Templates.TRADING_STATUS, {
     backLinkUrl
   });
@@ -18,7 +18,7 @@ export const post = (req: Request, res: Response) => {
   if (tradingStatus === "yes") {
     return res.redirect(url);
   } else if (tradingStatus === "no") {
-    const backLinkUrl = `/confirmation-statement/company/${companyNumber}/trading-status`;
+    const backLinkUrl = TRADING_STATUS_PATH.replace(`:${urlParams.PARAM_COMPANY_NUMBER}`, companyNumber);
     return res.render(Templates.TRADING_STOP, {
       backLinkUrl
     });
