79a3ef8a24b2ae1abb65f75b410df2ce8eda31e5
Jack Skinner <jskinner@companieshouse.gov.uk>
2021-06-09 15:51:28 +0100

    display correct filing date based on filing early or late or on time

--- src/controllers/task.list.controller.ts
+++ src/controllers/task.list.controller.ts
@@ -4,15 +4,29 @@ import { initTaskList, TaskList } from "../types/task.list";
 import { CompanyProfile } from "@companieshouse/api-sdk-node/dist/services/company-profile/types";
 import { getCompanyProfile } from "../services/company.profile.service";
 import { TRADING_STATUS_PATH, urlParams } from "../types/page.urls";
+import { isInFuture, toReadableFormat } from "../utils/date";
 
 export const get = async (req: Request, res: Response) => {
   const companyNumber = req.params[urlParams.PARAM_COMPANY_NUMBER];
   const backLinkUrl = TRADING_STATUS_PATH.replace(`:${urlParams.PARAM_COMPANY_NUMBER}`, companyNumber);
   const company: CompanyProfile = await getCompanyProfile(companyNumber);
   const taskList: TaskList = initTaskList();
+  taskList.recordDate = calculateFilingDate(taskList.recordDate, company);
   return res.render(Templates.TASK_LIST, {
     backLinkUrl,
     company,
     taskList
   });
+};
+
+const calculateFilingDate = (recordDate: string, companyProfile: CompanyProfile): string => {
+  const nextMadeUpToDate = companyProfile.confirmationStatement.nextMadeUpTo;
+  if (nextMadeUpToDate) {
+    if (isInFuture(nextMadeUpToDate)) {
+      return recordDate;
+    } else {
+      recordDate = toReadableFormat(companyProfile.confirmationStatement.nextDue);
+    }
+  }
+  return recordDate;
 };
--- test/controllers/task.list.controller.unit.ts
+++ test/controllers/task.list.controller.unit.ts
@@ -8,6 +8,8 @@ import app from "../../src/app";
 import { companyAuthenticationMiddleware } from "../../src/middleware/company.authentication.middleware";
 import { getCompanyProfile } from "../../src/services/company.profile.service";
 import { validCompanyProfile } from "../mocks/company.profile.mock";
+import { toReadableFormat } from "../../src/utils/date";
+import { DateTime } from "luxon";
 
 const mockCompanyAuthenticationMiddleware = companyAuthenticationMiddleware as jest.Mock;
 mockCompanyAuthenticationMiddleware.mockImplementation((req, res, next) => next());
@@ -25,4 +27,34 @@ describe("Task list controller tests", () => {
     const response = await request(app).get(url);
     expect(response.text).toContain("You will need to check and confirm that the company information we have on record is correct");
   });
+
+  it("Should show recordDate as next due date when filing after nextMadeUpToDate", async () => {
+    const expectedDate = toReadableFormat(validCompanyProfile.confirmationStatement.nextDue);
+    mockGetCompanyProfile.mockResolvedValueOnce(validCompanyProfile);
+    mocks.mockAuthenticationMiddleware.mockClear();
+    const url = TASK_LIST_PATH.replace(":companyNumber", COMPANY_NUMBER);
+    const response = await request(app).get(url);
+    expect(response.text).toContain(expectedDate);
+  });
+
+  it("Should show recordDate as next due date when filing on the nextMadeUpToDate", async () => {
+    validCompanyProfile.confirmationStatement.nextMadeUpTo = DateTime.now().toString();
+    const expectedDate = toReadableFormat(validCompanyProfile.confirmationStatement.nextDue);
+    mockGetCompanyProfile.mockResolvedValueOnce(validCompanyProfile);
+    mocks.mockAuthenticationMiddleware.mockClear();
+    const url = TASK_LIST_PATH.replace(":companyNumber", COMPANY_NUMBER);
+    const response = await request(app).get(url);
+    expect(response.text).toContain(expectedDate);
+  });
+
+  it("Should show recordDate as sysdate when filing before the nextMadeUpToDate", async () => {
+    validCompanyProfile.confirmationStatement.nextMadeUpTo = DateTime.fromISO('2999-06-04T00:00:00.000Z').toString();
+    const expectedDate = toReadableFormat(DateTime.now().toString());
+    mockGetCompanyProfile.mockResolvedValueOnce(validCompanyProfile);
+    mocks.mockAuthenticationMiddleware.mockClear();
+    const url = TASK_LIST_PATH.replace(":companyNumber", COMPANY_NUMBER);
+    const response = await request(app).get(url);
+    expect(response.text).toContain(expectedDate);
+  });
+
 });
