cfbb17de3dc68d80c519760ee232c80aeed738c4
markpit <mpitten@companieshouse.gov.uk>
2021-06-23 17:08:58 +0100

    Updating unit test for url utils

--- test/utils/url.unit.ts
+++ test/utils/url.unit.ts
@@ -1,3 +1,4 @@
+import { urlParams } from "../../src/types/page.urls";
 import { getUrlWithCompanyNumber } from "../../src/utils/url";
 
 describe("url utils tests", () => {
@@ -6,7 +7,7 @@ describe("url utils tests", () => {
     const COMPANY_NUMBER = "12345678";
 
     it("should populate a url with a company number", () => {
-      const url = "/something/:companyNumber/something";
+      const url = `/something/:${urlParams.PARAM_COMPANY_NUMBER}/something`;
       const populatedUrl = getUrlWithCompanyNumber(url, COMPANY_NUMBER);
       expect(populatedUrl).toEqual(`/something/${COMPANY_NUMBER}/something`);
     });
