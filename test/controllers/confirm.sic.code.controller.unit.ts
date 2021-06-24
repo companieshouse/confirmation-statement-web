583886f5dcab66aa5b9974c5878d18d57654e77f
kheseyongya <kheseyongya@gmail.com>
2021-06-24 12:00:56 +0100

    Amend unit test to reflect the url changes

--- test/controllers/confirm.sic.code.controller.unit.ts
+++ test/controllers/confirm.sic.code.controller.unit.ts
@@ -1,7 +1,15 @@
 import mocks from "../mocks/all.middleware.mock";
 import request from "supertest";
 import app from "../../src/app";
+import { SIC_PATH } from "../../src/types/page.urls";
+import { companyAuthenticationMiddleware } from "../../src/middleware/company.authentication.middleware";
 
+jest.mock("../../src/middleware/company.authentication.middleware");
+
+const mockCompanyAuthenticationMiddleware = companyAuthenticationMiddleware as jest.Mock;
+mockCompanyAuthenticationMiddleware.mockImplementation((req, res, next) => next());
+
+const COMPANY_NUMBER = "12345678";
 const EXPECTED_TEXT = "Check the SIC codes";
 
 describe("Confirm sic code controller tests", () => {
@@ -12,9 +20,8 @@ describe("Confirm sic code controller tests", () => {
 
   it("should return sic code page", async () => {
     mocks.mockAuthenticationMiddleware.mockClear();
-    const response = await request(app)
-      .get("/confirmation-statement/sic");
-
+    const url = SIC_PATH.replace(":companyNumber", COMPANY_NUMBER);
+    const response = await request(app).get(url);
     expect(response.text).toContain(EXPECTED_TEXT);
     expect(mocks.mockAuthenticationMiddleware).toHaveBeenCalled();
   });
