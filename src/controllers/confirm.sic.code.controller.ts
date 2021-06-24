69ae59a1da69c932676e510cc80ad208f5197859
kheseyongya <kheseyongya@gmail.com>
2021-06-24 11:23:19 +0100

    Amend url to incorporate back link

--- src/controllers/confirm.sic.code.controller.ts
+++ src/controllers/confirm.sic.code.controller.ts
@@ -1,9 +1,14 @@
 import { NextFunction, Request, Response } from "express";
+import { TASK_LIST_PATH, urlParams } from "../types/page.urls";
 import { Templates } from "../types/template.paths";
 
 export const get = (req: Request, res: Response, next: NextFunction) => {
   try {
-    return res.render(Templates.SIC);
+    const companyNumber = req.params[urlParams.PARAM_COMPANY_NUMBER];
+    const backLinkUrl = TASK_LIST_PATH.replace(`:${urlParams.PARAM_COMPANY_NUMBER}`, companyNumber);
+    return res.render(Templates.SIC, {
+      backLinkUrl
+    });
   } catch (e) {
     return next(e);
   }
