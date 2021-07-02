976d460f8fbec5e52a4bb0d5aab596a2dae637fd
Alfred Bwomezi <ABwomezi@companieshouse.gov.uk>
2021-07-02 10:24:22 +0100

    Display registered address.

--- src/controllers/tasks/registered.office.address.controller.ts
+++ src/controllers/tasks/registered.office.address.controller.ts
@@ -2,14 +2,19 @@ import { NextFunction, Request, Response } from "express";
 import { urlUtils } from "../../utils/url";
 import { TASK_LIST_PATH, urlParams } from "../../types/page.urls";
 import { Templates } from "../../types/template.paths";
+import { CompanyProfile } from "@companieshouse/api-sdk-node/dist/services/company-profile/types";
+import { getCompanyProfile } from "../../services/company.profile.service";
 
-export const get = (req: Request, res: Response, next: NextFunction) => {
+export const get = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const companyNumber = req.params[urlParams.PARAM_COMPANY_NUMBER];
+    const companyProfile: CompanyProfile = await getCompanyProfile(companyNumber);
     const backLinkUrl = urlUtils.getUrlWithCompanyNumber(TASK_LIST_PATH, companyNumber);
     return res.render(Templates.REGISTERED_OFFICE_ADDRESS, {
       templateName: Templates.REGISTERED_OFFICE_ADDRESS,
-      backLinkUrl });
+      backLinkUrl,
+      registeredOfficeAddress: companyProfile.registeredOfficeAddress
+    });
   } catch (error) {
     return next(error);
   }
--- test/controllers/tasks/registered.office.address.controller.unit.ts
+++ test/controllers/tasks/registered.office.address.controller.unit.ts
@@ -1,4 +1,5 @@
 jest.mock("../../../src/middleware/company.authentication.middleware");
+jest.mock("../../../src/services/company.profile.service");
 
 import request from "supertest";
 import mocks from "../../mocks/all.middleware.mock";
@@ -6,13 +7,16 @@ import { companyAuthenticationMiddleware } from "../../../src/middleware/company
 import app from "../../../src/app";
 import { REGISTERED_OFFICE_ADDRESS_PATH, urlParams } from "../../../src/types/page.urls";
 import { urlUtils } from "../../../src/utils/url";
+import { getCompanyProfile } from "../../../src/services/company.profile.service";
+import { validCompanyProfile } from "../../mocks/company.profile.mock";
 
 const mockCompanyAuthenticationMiddleware = companyAuthenticationMiddleware as jest.Mock;
 mockCompanyAuthenticationMiddleware.mockImplementation((req, res, next) => next());
+const mockGetCompanyProfile = getCompanyProfile as jest.Mock;
 
 const PAGE_HEADING = "Review the registered office address";
-
 const COMPANY_NUMBER = "12345678";
+
 const REGISTERED_OFFICE_ADDRESS_URL = REGISTERED_OFFICE_ADDRESS_PATH.replace(`:${urlParams.PARAM_COMPANY_NUMBER}`, COMPANY_NUMBER);
 
 describe("Registered Office Address controller tests", () => {
@@ -24,10 +28,14 @@ describe("Registered Office Address controller tests", () => {
   });
 
   it("Should navigate to the Registered Office Address page", async () => {
+    mockGetCompanyProfile.mockResolvedValueOnce(validCompanyProfile);
     const response = await request(app).get(REGISTERED_OFFICE_ADDRESS_URL);
 
     expect(response.text).toContain(PAGE_HEADING);
     expect(response.text).toContain("Check the registered office address");
+    expect(response.text).toContain(validCompanyProfile.registeredOfficeAddress.addressLineOne);
+    expect(response.text).toContain(validCompanyProfile.registeredOfficeAddress.addressLineTwo);
+    expect(response.text).toContain(validCompanyProfile.registeredOfficeAddress.postalCode);
   });
 
   it("Should redirect to an error page when error is thrown", async () => {
