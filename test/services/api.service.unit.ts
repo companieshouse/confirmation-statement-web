40cd285f87cda48a105a6d8bae064b60f0c10b4b
svarallo <svarallo@companieshouse.gov.uk>
2021-07-05 13:48:59 +0100

    ncs-449-adding-unit-tests-for-createPublicOAuthClient-method

--- test/services/api.service.unit.ts
+++ test/services/api.service.unit.ts
@@ -1,6 +1,6 @@
 jest.mock("../../src/utils/logger");
 
-import { createPrivateOAuthApiClient } from "../../src/services/api.service";
+import {createPrivateOAuthApiClient, createPublicOAuthApiClient} from "../../src/services/api.service";
 import { getEmptySessionRequest, getSessionRequest } from "../mocks/session.mock";
 import { Session } from "@companieshouse/node-session-handler";
 import { createAndLogError } from "../../src/utils/logger";
@@ -21,7 +21,12 @@ describe ("Test node session handler authorization for private sdk", () => {
     expect(client.confirmationStatementService).not.toBeNull();
   });
 
-  it("Should throw error when no data is present", () => {
+  it ("Should obtain public node sdk oauth client", () => {
+    const client = createPublicOAuthApiClient(getSessionRequest({ access_token: "token" }));
+    expect(client.transaction).not.toBeNull();
+  });
+
+  it("Should throw error when no data is present (Private OAuth Client)", () => {
     try {
       createPrivateOAuthApiClient({} as Session);
       fail();
@@ -30,7 +35,7 @@ describe ("Test node session handler authorization for private sdk", () => {
     }
   });
 
-  it("Should throw error when no sign in info is present", () => {
+  it("Should throw error when no sign in info is present (Private OAuth Client)", () => {
     try {
       const session: Session = getEmptySessionRequest();
       session.data = {};
@@ -41,9 +46,38 @@ describe ("Test node session handler authorization for private sdk", () => {
     }
   });
 
-  it("Should throw error when no access token is present", () => {
+  it("Should throw error when no access token is present (Private OAuth Client)", () => {
     try {
       createPrivateOAuthApiClient(getSessionRequest());
+      fail();
+    } catch (error) {
+      expect(mockCreateAndLogError).toBeCalledWith(ERROR_MESSSAGE);
+    }
+  });
+
+  it("Should throw error when no data is present (Public OAuth Client)", () => {
+    try {
+      createPublicOAuthApiClient({} as Session);
+      fail();
+    } catch (error) {
+      expect(mockCreateAndLogError).toBeCalledWith(ERROR_MESSSAGE);
+    }
+  });
+
+  it("Should throw error when no sign in info is present (Public OAuth Client)", () => {
+    try {
+      const session: Session = getEmptySessionRequest();
+      session.data = {};
+      createPublicOAuthApiClient(session);
+      fail();
+    } catch (error) {
+      expect(mockCreateAndLogError).toBeCalledWith(ERROR_MESSSAGE);
+    }
+  });
+
+  it("Should throw error when no access token is present (Public OAuth Client)", () => {
+    try {
+      createPublicOAuthApiClient(getSessionRequest());
       fail();
     } catch (error) {
       expect(mockCreateAndLogError).toBeCalledWith(ERROR_MESSSAGE);
