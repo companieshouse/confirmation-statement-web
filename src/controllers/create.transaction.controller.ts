2b08e1f0b97db03de87555142a24735fae63b9d5
svarallo <svarallo@companieshouse.gov.uk>
2021-07-07 13:13:44 +0100

    ncs-449-adding-create-submission

--- src/controllers/create.transaction.controller.ts
+++ src/controllers/create.transaction.controller.ts
@@ -4,16 +4,22 @@ import { urlUtils } from "../utils/url";
 import { TRADING_STATUS_PATH, urlParams } from "../types/page.urls";
 import { DESCRIPTION, REFERENCE } from "../utils/constants";
 import { Session } from "@companieshouse/node-session-handler";
+import {createConfirmationStatement} from "../services/confirmation.statement.service";
+import {Transaction} from "@companieshouse/api-sdk-node/dist/services/transaction/types";
 
 export const get = async (req: Request, res: Response, next: NextFunction) => {
 
   try {
     const session = req.session as Session;
     const companyNumber = req.params[urlParams.PARAM_COMPANY_NUMBER];
-    await postTransaction(session, companyNumber, DESCRIPTION, REFERENCE);
-
-    const nextPageUrl = urlUtils.getUrlWithCompanyNumber(TRADING_STATUS_PATH, companyNumber);
-    return res.redirect(nextPageUrl);
+    const transaction: Transaction = await postTransaction(session, companyNumber, DESCRIPTION, REFERENCE);
+    const transactionId = transaction.id as string;
+    const submissionResponse = await createConfirmationStatement(session, transactionId)
+    if (submissionResponse.httpStatusCode === 201) {
+      const nextPageUrl = urlUtils.getUrlWithCompanyNumber(TRADING_STATUS_PATH, companyNumber);
+      return res.redirect(nextPageUrl);
+    }
+    return next(new Error("Company is ineligible " + JSON.stringify(submissionResponse)));
   } catch (e) {
     return next(e);
   }
--- src/services/confirmation.statement.service.ts
+++ src/services/confirmation.statement.service.ts
@@ -1,12 +1,22 @@
 import { Session } from "@companieshouse/node-session-handler";
-import { ConfirmationStatementService } from "private-api-sdk-node/dist/services/confirmation-statement";
-import Resource from "@companieshouse/api-sdk-node/dist/services/resource";
+import {
+  CompanyValidationResponse,
+  ConfirmationStatementCreated,
+  ConfirmationStatementService
+} from "private-api-sdk-node/dist/services/confirmation-statement";
+import Resource, {ApiErrorResponse} from "@companieshouse/api-sdk-node/dist/services/resource";
 import { createPrivateOAuthApiClient } from "./api.service";
 
 export const createConfirmationStatement = async (session: Session,
-                                                  transactionId: string): Promise<Resource<any>> => {
+                                                  transactionId: string): Promise<Resource<ConfirmationStatementCreated | CompanyValidationResponse>> => {
   const client = createPrivateOAuthApiClient(session);
   const csService: ConfirmationStatementService = client.confirmationStatementService;
-  return await csService.postNewConfirmationStatement(transactionId);
+  const response = await csService.postNewConfirmationStatement(transactionId);
+  if (response.httpStatusCode != (201 || 400)) {
+    const castedResponse: ApiErrorResponse = response;
+    throw new Error("Something went wrong creating confirmation statement " + JSON.stringify(castedResponse))
+  } else {
+    return response as Resource<ConfirmationStatementCreated | CompanyValidationResponse>
+  }
 };
 
