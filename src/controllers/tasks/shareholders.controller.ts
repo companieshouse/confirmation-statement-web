017b38ceab0689a4002b2fa7049ec77320d7d599
Lewis Evans <levans@companieshouse.gov.uk>
2021-07-19 14:30:48 +0100

    shareholder stop page

--- src/controllers/tasks/shareholders.controller.ts
+++ src/controllers/tasks/shareholders.controller.ts
@@ -1,10 +1,23 @@
 import { Templates } from "../../types/template.paths";
 import { Request, Response } from "express";
-import { TASK_LIST_PATH, urlParams } from "../../types/page.urls";
+import { TASK_LIST_PATH, urlParams, SHAREHOLDERS_PATH } from "../../types/page.urls";
 import { urlUtils } from "../../utils/url";
+import { RADIO_BUTTON_VALUE } from "../../utils/constants";
 
 export const get = (req: Request, res: Response) => {
   const companyNumber = req.params[urlParams.PARAM_COMPANY_NUMBER];
   const backLinkUrl = urlUtils.getUrlWithCompanyNumber(TASK_LIST_PATH, companyNumber);
   return res.render(Templates.SHAREHOLDERS, { backLinkUrl });
+};
+
+export const post = (req: Request, res: Response) => {
+  const shareholderButtonValue = req.body.shareholders;
+  const companyNumber = req.params[urlParams.PARAM_COMPANY_NUMBER];
+
+  if (shareholderButtonValue === RADIO_BUTTON_VALUE.NO) {
+    return res.render(Templates.WRONG_SHAREHOLDERS, {
+      backLinkUrl: urlUtils.getUrlWithCompanyNumber(SHAREHOLDERS_PATH, companyNumber),
+      templateName: Templates.WRONG_SHAREHOLDERS,
+    });
+  }
 };
