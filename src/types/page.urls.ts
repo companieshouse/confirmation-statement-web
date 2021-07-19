e84fa5b7b9455fda86d0855819be5e575843d613
ICoxCH <ICox@companieshouse.gov.uk>
2021-07-19 10:31:01 +0100

    WIP - add transaction id and submission id to url

--- src/types/page.urls.ts
+++ src/types/page.urls.ts
@@ -1,12 +1,16 @@
 import { Templates } from "./template.paths";
 
 export enum urlParams {
-  PARAM_COMPANY_NUMBER = "companyNumber"
+  PARAM_COMPANY_NUMBER = "companyNumber",
+  PARAM_TRANSACTION_ID = "transactionId",
+  PARAM_SUBMISSION_ID = "submissionId"
 }
 
 const SEPARATOR = "/";
 
 export const COMPANY_AUTH_PROTECTED_BASE = `/company/:${urlParams.PARAM_COMPANY_NUMBER}/`;
+export const ACTIVE_SUBMISSION_BASE = COMPANY_AUTH_PROTECTED_BASE +
+  `transaction/:${urlParams.PARAM_TRANSACTION_ID}/submission/:${urlParams.PARAM_SUBMISSION_ID}/`;
 
 export const ACCESSIBILITY_STATEMENT = SEPARATOR + Templates.ACCESSIBILITY_STATEMENT;
 export const CONFIRM_COMPANY = SEPARATOR + Templates.CONFIRM_COMPANY;
@@ -18,7 +22,7 @@ export const SIC = COMPANY_AUTH_PROTECTED_BASE + "sic";
 export const SIC_PATH = CONFIRMATION_STATEMENT + SIC;
 export const CREATE_TRANSACTION = COMPANY_AUTH_PROTECTED_BASE + "transaction";
 export const CREATE_TRANSACTION_PATH = CONFIRMATION_STATEMENT + CREATE_TRANSACTION;
-export const TRADING_STATUS = COMPANY_AUTH_PROTECTED_BASE + "trading-status";
+export const TRADING_STATUS = ACTIVE_SUBMISSION_BASE + "trading-status";
 export const TRADING_STATUS_PATH = CONFIRMATION_STATEMENT + TRADING_STATUS ;
 export const TASK_LIST = COMPANY_AUTH_PROTECTED_BASE + "task-list";
 export const TASK_LIST_PATH = CONFIRMATION_STATEMENT + TASK_LIST;
--- src/utils/url.ts
+++ src/utils/url.ts
@@ -3,6 +3,15 @@ import { urlParams } from "../types/page.urls";
 const getUrlWithCompanyNumber = (url: string, companyNumber: string): string =>
   url.replace(`:${urlParams.PARAM_COMPANY_NUMBER}`, companyNumber);
 
+const getUrlWithCompanyNumberTransactionIdAndSubmissionId = (url: string, companyNumber: string,
+                                                             transactionId: string, submissionId: string): string => {
+  url = url.replace(`:${urlParams.PARAM_COMPANY_NUMBER}`, companyNumber);
+  url = url.replace(`:${urlParams.PARAM_TRANSACTION_ID}`, transactionId);
+  url = url.replace(`:${urlParams.PARAM_SUBMISSION_ID}`, submissionId);
+  return url;
+};
+
 export const urlUtils = {
-  getUrlWithCompanyNumber
+  getUrlWithCompanyNumber,
+  getUrlWithCompanyNumberTransactionIdAndSubmissionId
 };
