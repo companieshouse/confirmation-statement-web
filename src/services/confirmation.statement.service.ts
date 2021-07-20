cd2ada9a7607d6d3f6a50aa131f81f036908235a
Matthew Culpin <mculpin@companieshouse.gov.uk>
2021-07-20 09:37:02 +0100

    NCS-506 Create new cs statment - should be no changes

--- src/services/confirmation.statement.service.ts
+++ src/services/confirmation.statement.service.ts
@@ -1,15 +1,24 @@
 import { Session } from "@companieshouse/node-session-handler";
 import {
+  CompanyValidationResponse,
+  ConfirmationStatementCreated,
   ConfirmationStatementService,
   ConfirmationStatementSubmission
 } from "private-api-sdk-node/dist/services/confirmation-statement";
+import Resource, { ApiErrorResponse } from "@companieshouse/api-sdk-node/dist/services/resource";
 import { createPrivateOAuthApiClient } from "./api.service";
 
 export const createConfirmationStatement = async (session: Session,
-                                                  transactionId: string): Promise<any> => {
+                                                  transactionId: string): Promise<Resource<ConfirmationStatementCreated | CompanyValidationResponse>> => {
   const client = createPrivateOAuthApiClient(session);
   const csService: ConfirmationStatementService = client.confirmationStatementService;
-  return await csService.postNewConfirmationStatement(transactionId);
+  const response = await csService.postNewConfirmationStatement(transactionId);
+  if (response.httpStatusCode !== 201 && response.httpStatusCode !== 400) {
+    const castedResponse: ApiErrorResponse = response;
+    throw new Error(`Something went wrong creating confirmation statement ${JSON.stringify(castedResponse)}`);
+  } else {
+    return response as Resource<ConfirmationStatementCreated | CompanyValidationResponse>;
+  }
 };
 
 export const updateConfirmationStatement = async (session: Session,
