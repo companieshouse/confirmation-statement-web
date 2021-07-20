5a2770a793521d5ce5a1cbb454d09cfedc9e77d7
Matthew Culpin <mculpin@companieshouse.gov.uk>
2021-07-20 09:23:34 +0100

    NCS-506 Update statement

--- src/controllers/tasks/statement.of.capital.controller.ts
+++ src/controllers/tasks/statement.of.capital.controller.ts
@@ -5,19 +5,27 @@ import { Templates } from "../../types/template.paths";
 import { urlUtils } from "../../utils/url";
 import { getStatementOfCapitalData } from "../../services/statement.of.capital.service";
 import { Session } from "@companieshouse/node-session-handler";
-import { StatementOfCapital } from "private-api-sdk-node/dist/services/confirmation-statement";
+import {
+  ConfirmationStatementSubmission,
+  SectionStatus,
+  StatementOfCapital
+} from "private-api-sdk-node/dist/services/confirmation-statement";
 import { formatTitleCase } from "../../utils/format";
+import { updateConfirmationStatement } from "../../services/confirmation.statement.service";
 
 export const get = async(req: Request, res: Response, next: NextFunction) => {
   try {
     const companyNumber = getCompanyNumber(req);
     const session: Session = req.session as Session;
     const statementOfCapital: StatementOfCapital = await getStatementOfCapitalData(session, companyNumber);
-    statementOfCapital.classOfShares = formatTitleCase(statementOfCapital.classOfShares);
+    if (statementOfCapital.classOfShares) {
+      statementOfCapital.classOfShares = formatTitleCase(statementOfCapital.classOfShares);
+    }
     return res.render(Templates.STATEMENT_OF_CAPITAL, {
       templateName: Templates.STATEMENT_OF_CAPITAL,
       backLinkUrl: urlUtils.getUrlWithCompanyNumber(TASK_LIST_PATH, companyNumber),
-      statementOfCapital: statementOfCapital
+      statementOfCapital: statementOfCapital,
+      statementOfCapitalJSON: JSON.stringify(statementOfCapital)
     });
   } catch (e) {
     return next(e);
@@ -26,10 +34,17 @@ export const get = async(req: Request, res: Response, next: NextFunction) => {
 
 export const post = (req: Request, res: Response, next: NextFunction) => {
   try {
-    const statementOfCapitalButtonValue = req.body.statementOfCapital;
+    const statementOfCapitalButtonValue = req.body.statementOfCapitalCorrect;
     const companyNumber = getCompanyNumber(req);
 
+    const statementOfCapitalContent: StatementOfCapital = JSON.parse(req.body.statementOfCapitalContent);
+    const transactionId = req.params.transactionId as string;
+    const submissionId = req.params.submissionId as string;
+
     if (statementOfCapitalButtonValue === RADIO_BUTTON_VALUE.YES) {
+      const session = req.session as Session;
+      const csSubmission = buildCsSubmission(submissionId, statementOfCapitalContent, SectionStatus.CONFIRMED);
+      updateConfirmationStatement(session, transactionId, submissionId, csSubmission);
       return res.redirect(urlUtils.getUrlWithCompanyNumber(TASK_LIST_PATH, companyNumber));
     } else if (statementOfCapitalButtonValue === RADIO_BUTTON_VALUE.NO) {
       return res.render(Templates.WRONG_STATEMENT_OF_CAPITAL, {
@@ -47,5 +62,22 @@ export const post = (req: Request, res: Response, next: NextFunction) => {
     return next(e);
   }
 };
+
+const buildCsSubmission = (submissionId: string, statementOfCapital: StatementOfCapital, status: SectionStatus):
+      ConfirmationStatementSubmission => {
+  return {
+    id: submissionId,
+    data: {
+      statementOfCapitalData: {
+        sectionStatus: status,
+        statementOfCapital: statementOfCapital
+      }
+    },
+    links: {
+      self: ""
+    }
+  };
+};
+
 
 const getCompanyNumber = (req: Request): string => req.params[urlParams.PARAM_COMPANY_NUMBER];
--- src/services/confirmation.statement.service.ts
+++ src/services/confirmation.statement.service.ts
@@ -1,22 +1,22 @@
 import { Session } from "@companieshouse/node-session-handler";
 import {
-  CompanyValidationResponse,
-  ConfirmationStatementCreated,
-  ConfirmationStatementService
+  ConfirmationStatementService,
+  ConfirmationStatementSubmission
 } from "private-api-sdk-node/dist/services/confirmation-statement";
-import Resource, { ApiErrorResponse } from "@companieshouse/api-sdk-node/dist/services/resource";
 import { createPrivateOAuthApiClient } from "./api.service";
 
 export const createConfirmationStatement = async (session: Session,
-                                                  transactionId: string): Promise<Resource<ConfirmationStatementCreated | CompanyValidationResponse>> => {
+                                                  transactionId: string): Promise<any> => {
   const client = createPrivateOAuthApiClient(session);
   const csService: ConfirmationStatementService = client.confirmationStatementService;
-  const response = await csService.postNewConfirmationStatement(transactionId);
-  if (response.httpStatusCode !== 201 && response.httpStatusCode !== 400) {
-    const castedResponse: ApiErrorResponse = response;
-    throw new Error(`Something went wrong creating confirmation statement ${JSON.stringify(castedResponse)}`);
-  } else {
-    return response as Resource<ConfirmationStatementCreated | CompanyValidationResponse>;
-  }
+  return await csService.postNewConfirmationStatement(transactionId);
 };
 
+export const updateConfirmationStatement = async (session: Session,
+                                                  transactionId: string,
+                                                  confirmationStatementId: string,
+                                                  csSubmission: ConfirmationStatementSubmission): Promise<any> => {
+  const client = createPrivateOAuthApiClient(session);
+  const csService: ConfirmationStatementService = client.confirmationStatementService;
+  return await csService.postUpdateConfirmationStatement(transactionId, confirmationStatementId, csSubmission);
+};
