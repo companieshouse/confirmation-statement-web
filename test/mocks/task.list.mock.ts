85b91234b8287a80fd262f8035aab35359597a5e
markpit <mpitten@companieshouse.gov.uk>
2021-07-23 16:38:54 +0100

    NCS-480 Updating task list controller test to use a mocked task list

---
+++ test/mocks/task.list.mock.ts
@@ -0,0 +1,44 @@
+import { TaskList, TaskState } from "../../src/types/task.list";
+
+export const mockTaskList: TaskList = {
+  tasks: {
+    sicCodes: {
+      state: TaskState.CHECKED,
+      isVisible: true,
+      url: "/sic-code",
+    },
+    statementOfCapital: {
+      state: TaskState.NOT_CHECKED,
+      isVisible: false,
+      url: "/soc"
+    },
+    officers: {
+      state: TaskState.IN_PROGRESS,
+      isVisible: true,
+      url: "/officers"
+    },
+    peopleSignificantControl: {
+      state: TaskState.CHECKED,
+      isVisible: true,
+      url: "/psc"
+    },
+    shareholders: {
+      state: TaskState.IN_PROGRESS,
+      isVisible: true,
+      url: "/shr",
+    },
+    registeredOfficeAddress: {
+      state: TaskState.NOT_CHECKED,
+      isVisible: true,
+      url: "/regaddr"
+    },
+    registerLocations: {
+      state: TaskState.NOT_CHECKED,
+      isVisible: false,
+    },
+  },
+  recordDate: "16 Jun 2021",
+  tasksCompletedCount: 2,
+  allTasksCompleted: false,
+  csDue: true,
+};
