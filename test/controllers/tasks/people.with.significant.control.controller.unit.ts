b9c926b26ec1262a6e3fb42a21a55a00afc7b7c4
svarallo <svarallo@companieshouse.gov.uk>
2021-07-27 17:00:09 +0100

    ncs-541-adding-error-to-psc-page-when-no-radio-chosen

--- test/controllers/tasks/people.with.significant.control.controller.unit.ts
+++ test/controllers/tasks/people.with.significant.control.controller.unit.ts
@@ -1,4 +1,4 @@
-import {urlUtils} from "../../../src/utils/url";
+import { urlUtils } from "../../../src/utils/url";
 
 jest.mock("../../../src/middleware/company.authentication.middleware");
 
@@ -7,10 +7,13 @@ import { PEOPLE_WITH_SIGNIFICANT_CONTROL_PATH } from "../../../src/types/page.ur
 import request from "supertest";
 import app from "../../../src/app";
 import { companyAuthenticationMiddleware } from "../../../src/middleware/company.authentication.middleware";
+import { PEOPLE_WITH_SIGNIFICANT_CONTROL_ERROR } from "../../../src/utils/constants";
 
 const mockCompanyAuthenticationMiddleware = companyAuthenticationMiddleware as jest.Mock;
 mockCompanyAuthenticationMiddleware.mockImplementation((req, res, next) => next());
 
+const PAGE_TITLE = "Review the people with significant control";
+const PAGE_HEADING = "Check the people with significant control (PSC)";
 const COMPANY_NUMBER = "12345678";
 const PEOPLE_WITH_SIGNIFICANT_CONTROL_URL = PEOPLE_WITH_SIGNIFICANT_CONTROL_PATH.replace(":companyNumber", COMPANY_NUMBER);
 
@@ -20,19 +23,32 @@ describe("People with significant control controller tests", () => {
     mocks.mockAuthenticationMiddleware.mockClear();
   });
 
-  it("should navigate to the active pscs page", async () => {
-    const response = await request(app).get(PEOPLE_WITH_SIGNIFICANT_CONTROL_URL);
-    expect(response.text).toContain("Check the people with significant control (PSC)");
+  describe("get tests", function () {
+    it("should navigate to the active pscs page", async () => {
+      const response = await request(app).get(PEOPLE_WITH_SIGNIFICANT_CONTROL_URL);
+      expect(response.text).toContain(PAGE_HEADING);
+    });
+
+    it("Should navigate to an error page if the function throws an error", async () => {
+      const spyGetUrlWithCompanyNumber = jest.spyOn(urlUtils, "getUrlWithCompanyNumberTransactionIdAndSubmissionId");
+      spyGetUrlWithCompanyNumber.mockImplementationOnce(() => { throw new Error(); });
+
+      const response = await request(app).get(PEOPLE_WITH_SIGNIFICANT_CONTROL_URL);
+
+      expect(response.text).toContain("Sorry, the service is unavailable");
+
+      spyGetUrlWithCompanyNumber.mockRestore();
+    });
   });
 
-  it("Should navigate to an error page if the function throws an error", async () => {
-    const spyGetUrlWithCompanyNumber = jest.spyOn(urlUtils, "getUrlWithCompanyNumberTransactionIdAndSubmissionId");
-    spyGetUrlWithCompanyNumber.mockImplementationOnce(() => { throw new Error(); });
+  describe("post tests", function () {
+    it("Should redisplay psc page with error when radio button is not selected", async () => {
+      const response = await request(app).post(PEOPLE_WITH_SIGNIFICANT_CONTROL_URL);
 
-    const response = await request(app).get(PEOPLE_WITH_SIGNIFICANT_CONTROL_URL);
-
-    expect(response.text).toContain("Sorry, the service is unavailable");
-
-    spyGetUrlWithCompanyNumber.mockRestore();
+      expect(response.status).toEqual(200);
+      expect(response.text).toContain(PAGE_TITLE);
+      expect(response.text).toContain(PEOPLE_WITH_SIGNIFICANT_CONTROL_ERROR);
+      expect(response.text).toContain(PAGE_HEADING);
+    });
   });
 });
