e8fdb863f51ef26e7fcce4517f307a0602397816
Alfred Bwomezi <ABwomezi@companieshouse.gov.uk>
2021-07-29 12:05:15 +0100

    ncs-559 Fix broken nav url links.

--- src/controllers/tasks/confirm.sic.code.controller.ts
+++ src/controllers/tasks/confirm.sic.code.controller.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { urlUtils } from "../../utils/url";
-import { TASK_LIST_PATH, urlParams, SIC_PATH } from "../../types/page.urls";
+import { TASK_LIST_PATH, SIC_PATH } from "../../types/page.urls";
 import { Templates } from "../../types/template.paths";
 import { lookupSicCodeDescription } from "../../utils/api.enumerations";
 import { CompanyProfile } from "@companieshouse/api-sdk-node/dist/services/company-profile/types";
@@ -10,8 +10,8 @@ import { RADIO_BUTTON_VALUE, SIC_CODE_ERROR } from "../../utils/constants";
 
 export const get = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const companyNumber = getCompanyNumber(req);
-    const backLinkUrl = urlUtils.getUrlWithCompanyNumber(TASK_LIST_PATH, companyNumber);
+    const companyNumber = urlUtils.getCompanyNumberFromRequestParams(req);
+    const backLinkUrl = urlUtils.getUrlToPath(TASK_LIST_PATH, req);
     const companyProfile: CompanyProfile = await getCompanyProfile(companyNumber);
     const sicCodes: SicCode[] = getSicCodeDetails(companyProfile);
     return res.render(Templates.SIC, { backLinkUrl, sicCodes, templateName: Templates.SIC });
@@ -22,24 +22,24 @@ export const get = async (req: Request, res: Response, next: NextFunction) => {
 
 export const post = async (req: Request, res: Response, next: NextFunction) => {
   const sicButtonValue = req.body.sicCodeStatus;
-  const companyNumber = getCompanyNumber(req);
 
   if (sicButtonValue === RADIO_BUTTON_VALUE.YES) {
-    return res.redirect(urlUtils.getUrlWithCompanyNumber(TASK_LIST_PATH, companyNumber));
+    return res.redirect(urlUtils.getUrlToPath(TASK_LIST_PATH, req));
   }
 
   if (sicButtonValue === RADIO_BUTTON_VALUE.NO) {
     return res.render(Templates.WRONG_SIC, {
-      backLinkUrl: urlUtils.getUrlWithCompanyNumber(SIC_PATH, companyNumber),
+      backLinkUrl: urlUtils.getUrlToPath(SIC_PATH, req),
       templateName: Templates.SIC,
       sickCodeStatus: sicButtonValue
     });
   }
 
   try {
+    const companyNumber = urlUtils.getCompanyNumberFromRequestParams(req);
     const companyProfile: CompanyProfile = await getCompanyProfile(companyNumber);
     return res.render(Templates.SIC, {
-      backLinkUrl: urlUtils.getUrlWithCompanyNumber(TASK_LIST_PATH, companyNumber),
+      backLinkUrl: urlUtils.getUrlToPath(TASK_LIST_PATH, req),
       sicCodes: getSicCodeDetails(companyProfile),
       sicCodeErrorMsg: SIC_CODE_ERROR,
       templateName: Templates.SIC
@@ -59,4 +59,3 @@ function getSicCodeDetails (companyProfile: CompanyProfile): SicCode[] {
   return sicCodeList;
 }
 
-const getCompanyNumber = (req: Request): string => req.params[urlParams.PARAM_COMPANY_NUMBER];
