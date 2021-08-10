ef1f7e572e965b2c0bce7347987daed1e67774fc
Matthew Culpin <mculpin@companieshouse.gov.uk>
2021-08-10 08:59:53 +0100

    NCS-582 No button navigation

--- src/controllers/tasks/psc.statement.controller.ts
+++ src/controllers/tasks/psc.statement.controller.ts
@@ -1,9 +1,8 @@
 import { NextFunction, Request, Response } from "express";
-import { PSC_STATEMENT_CONTROL_ERROR, PSC_STATEMENT_NOT_FOUND } from "../../utils/constants";
-import { PEOPLE_WITH_SIGNIFICANT_CONTROL_PATH } from "../../types/page.urls";
+import { PSC_STATEMENT_CONTROL_ERROR, PSC_STATEMENT_NOT_FOUND, RADIO_BUTTON_VALUE } from "../../utils/constants";
+import { PEOPLE_WITH_SIGNIFICANT_CONTROL_PATH, PSC_STATEMENT_PATH } from "../../types/page.urls";
 import { Templates } from "../../types/template.paths";
 import { urlUtils } from "../../utils/url";
-
 
 export const get = (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -23,6 +22,15 @@ export const get = (req: Request, res: Response, next: NextFunction) => {
 export const post = (req: Request, res: Response, next: NextFunction) => {
   try {
     const pscButtonValue = req.body.pscStatementValue;
+
+    if (pscButtonValue === RADIO_BUTTON_VALUE.NO) {
+      return res.render(Templates.WRONG_DETAILS, {
+        templateName: Templates.WRONG_DETAILS,
+        backLinkUrl: urlUtils.getUrlToPath(PSC_STATEMENT_PATH, req),
+        stepOneHeading: "Update the people with significant control (PSC) details",
+        pageHeading: "Incorrect people with significant control - File a confirmation statement",
+      });
+    }
 
     if (!pscButtonValue) {
       const pscStatement = PSC_STATEMENT_NOT_FOUND;
--- test/controllers/tasks/psc.statement.controller.unit.ts
+++ test/controllers/tasks/psc.statement.controller.unit.ts
@@ -3,10 +3,11 @@ import request from "supertest";
 import app from "../../../src/app";
 import { urlUtils } from "../../../src/utils/url";
 import { PSC_STATEMENT_PATH } from "../../../src/types/page.urls";
-import { PSC_STATEMENT_CONTROL_ERROR } from "../../../src/utils/constants";
+import { PSC_STATEMENT_CONTROL_ERROR, RADIO_BUTTON_VALUE } from "../../../src/utils/constants";
 
 const PAGE_TITLE = "Review the people with significant control";
 const PAGE_HEADING = "Is the PSC statement correct?";
+const STOP_PAGE_HEADING = "";
 const COMPANY_NUMBER = "12345678";
 const TRANSACTION_ID = "66544";
 const SUBMISSION_ID = "6464647";
@@ -53,6 +54,15 @@ describe("PSC Statement controller tests", () => {
       expect(response.text).toContain(PAGE_TITLE);
       expect(response.text).toContain(PSC_STATEMENT_CONTROL_ERROR);
       expect(response.text).toContain(PAGE_HEADING);
+    });
+
+    it("Should display wrong psc data page when no radio button is selected", async () => {
+      const response = await request(app)
+        .post(PSC_STATEMENT_URL)
+        .send({ pscRadioValue: RADIO_BUTTON_VALUE.NO });
+
+      expect(response.status).toEqual(200);
+      expect(response.text).toContain(STOP_PAGE_HEADING);
     });
 
 
