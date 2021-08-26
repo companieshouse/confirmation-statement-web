9bfbbfd50e62387f960fe6c4c55a3e25d3294cef
CHJSkinner <jskinner@companieshouse.go.uk>
2021-08-26 11:38:59 +0100

    remove unused import

--- src/controllers/tasks/register.locations.controller.ts
+++ src/controllers/tasks/register.locations.controller.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { urlUtils } from "../../utils/url";
-import { TASK_LIST_PATH, urlParams } from "../../types/page.urls";
+import { TASK_LIST_PATH } from "../../types/page.urls";
 import { Templates } from "../../types/template.paths";
 
 export const get = (req: Request, res: Response, next: NextFunction) => {
