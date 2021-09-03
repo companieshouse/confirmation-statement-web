a0f2ba7326c39a21870d979598f3261fcac23ef5
Lewis Evans <levans@companieshouse.gov.uk>
2021-09-03 07:35:00 +0100

    ncs 338 display register data inital commit

--- src/controllers/tasks/register.locations.controller.ts
+++ src/controllers/tasks/register.locations.controller.ts
@@ -2,14 +2,43 @@ import { NextFunction, Request, Response } from "express";
 import { urlUtils } from "../../utils/url";
 import { TASK_LIST_PATH } from "../../types/page.urls";
 import { Templates } from "../../types/template.paths";
+import { Session } from "@companieshouse/node-session-handler";
+import { getRegisterLocationData } from "../../services/register.location.service";
+import { RegisterLocation } from "private-api-sdk-node/dist/services/confirmation-statement";
+import { formatAddress, formatAddressForDisplay } from "../../utils/format";
 
-export const get = (req: Request, res: Response, next: NextFunction) => {
+export const get = async (req: Request, res: Response, next: NextFunction) => {
   try {
+    const companyNumber = urlUtils.getCompanyNumberFromRequestParams(req);
+    const session: Session = req.session as Session;
+    const registerLocations: RegisterLocation[] = await getRegisterLocationData(session, companyNumber);
+    console.log("JSON OBJECT: " + JSON.stringify(registerLocations));
+    const sailAddress = formatSailForDisplay(registerLocations);
+    const registers = formatRegisterForDisplay(registerLocations);
+    console.log("registers: " + registers + " length: " + registers.length);
     const backLinkUrl = urlUtils.getUrlToPath(TASK_LIST_PATH, req);
     return res.render(Templates.REGISTER_LOCATIONS, {
       templateName: Templates.REGISTER_LOCATIONS,
-      backLinkUrl });
+      backLinkUrl, registers, sailAddress });
   } catch (error) {
     return next(error);
   }
+};
+
+const formatSailForDisplay = (regLoc: RegisterLocation[]): string => {
+  let sailAddress = "";
+  if (regLoc.length && regLoc[0].sailAddress) {
+    sailAddress = formatAddressForDisplay(formatAddress(regLoc[0].sailAddress));
+  }
+  return sailAddress;
+};
+
+const formatRegisterForDisplay = (regLoc: RegisterLocation[]): Array<string> => {
+  const registers = new Array<string>();
+  for (const key of Object.values(regLoc)) {
+    if (key.registerTypeDesc) {
+      registers.push(key.registerTypeDesc);
+    }
+  }
+  return registers;
 };
---
+++ src/services/register.location.service.ts
@@ -0,0 +1,18 @@
+import { Resource } from "@companieshouse/api-sdk-node";
+import { ApiErrorResponse } from "@companieshouse/api-sdk-node/dist/services/resource";
+import { Session } from "@companieshouse/node-session-handler";
+import { RegisterLocation, ConfirmationStatementService } from "private-api-sdk-node/dist/services/confirmation-statement";
+import { createPrivateOAuthApiClient } from "./api.service";
+
+export const getRegisterLocationData = async (session: Session, companyNumber: string): Promise<RegisterLocation[]> => {
+  const client = createPrivateOAuthApiClient(session);
+  const csService: ConfirmationStatementService = client.confirmationStatementService;
+  const response: Resource<RegisterLocation[]> | ApiErrorResponse = await csService.getRegisterLocations(companyNumber);
+  const status = response.httpStatusCode as number;
+  if (status >= 400) {
+    const errorResponse = response as ApiErrorResponse;
+    throw new Error(`Error retrieving register location data from confirmation-statment api: ${JSON.stringify(errorResponse)}`);
+  }
+  const successfulResponse = response as Resource<RegisterLocation[]>;
+  return successfulResponse.resource as RegisterLocation[];
+};
