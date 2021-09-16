ce6400d5675a262f5d9ab9415e8d6722cb84e8a7
Matthew Culpin <mculpin@companieshouse.gov.uk>
2021-09-16 16:36:20 +0100

    NCS-676 Logging update

--- src/controllers/payment.callback.controller.ts
+++ src/controllers/payment.callback.controller.ts
@@ -6,11 +6,12 @@ import { CONFIRMATION_PATH, REVIEW_PATH } from "../types/page.urls";
 export const get = (req: Request, res: Response, next: NextFunction) => {
   try {
     const paymentStatus = req.query.status;
+    const submissionId = urlUtils.getSubmissionIdFromRequestParams(req);
     if (paymentStatus === "paid") {
-      logger.info("Payment status: " + paymentStatus + " - redirecting to the confirmation page");
+      logger.debug("Submission id: " + submissionId + " - Payment status: " + paymentStatus + " - redirecting to the confirmation page");
       return res.redirect(urlUtils.getUrlToPath(CONFIRMATION_PATH, req));
     } else {
-      logger.info("Payment status: " + paymentStatus + " - redirecting to the review page");
+      logger.debug("Submission id: " + submissionId + " - Payment status: " + paymentStatus + " - redirecting to the review page");
       return res.redirect(urlUtils.getUrlToPath(REVIEW_PATH, req));
     }
   } catch (e) {
