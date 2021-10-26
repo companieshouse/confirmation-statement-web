860c867d1ab59377433c180bf21c7a4793eee29e
ICoxCH <ICox@companieshouse.gov.uk>
2021-10-26 10:51:23 +0100

    Sonar reccomends against declaring then immediately returning a var

--- src/utils/update.confirmation.statement.submission.ts
+++ src/utils/update.confirmation.statement.submission.ts
@@ -2,19 +2,13 @@ import { Request } from "express";
 import { Session } from "@companieshouse/node-session-handler";
 import {
   ConfirmationStatementSubmission,
-  RegisteredOfficeAddressData,
-  ActiveDirectorDetailsData,
   SectionStatus,
-  PersonsOfSignificantControlData,
-  SicCodeData,
   StatementOfCapitalData,
-  ShareholderData,
-  RegisterLocationsData,
   TradingStatusData
 } from "@companieshouse/api-sdk-node/dist/services/confirmation-statement";
 import { getConfirmationStatement, updateConfirmationStatement } from "../services/confirmation.statement.service";
-import { SECTIONS } from "../utils/constants";
-import { urlUtils } from "../utils/url";
+import { SECTIONS } from "./constants";
+import { urlUtils } from "./url";
 
 export const sendUpdate = async (req: Request, sectionName: SECTIONS, status: SectionStatus, extraData?: any ) => {
   const transactionId = urlUtils.getTransactionIdFromRequestParams(req);
@@ -46,28 +40,24 @@ const updateCsSubmission = (currentCsSubmission: ConfirmationStatementSubmission
 const generateSectionData = (section: SECTIONS, status: SectionStatus, extraData?: any): any => {
   switch (section) {
       case SECTIONS.ACTIVE_DIRECTOR: {
-        const newData: ActiveDirectorDetailsData = {
+        return {
           sectionStatus: status,
         };
-        return newData;
       }
       case SECTIONS.PSC: {
-        const newPSCData: PersonsOfSignificantControlData = {
+        return {
           sectionStatus: status
         };
-        return newPSCData;
       }
       case SECTIONS.ROA: {
-        const newRoaData: RegisteredOfficeAddressData = {
+        return {
           sectionStatus: status,
         };
-        return newRoaData;
       }
       case SECTIONS.SIC: {
-        const newSicData: SicCodeData = {
+        return {
           sectionStatus: status
         };
-        return newSicData;
       }
       case SECTIONS.SOC: {
         const newSocData: StatementOfCapitalData = {
@@ -79,16 +69,14 @@ const generateSectionData = (section: SECTIONS, status: SectionStatus, extraData
         return newSocData;
       }
       case SECTIONS.SHAREHOLDER: {
-        const newShareholderData: ShareholderData = {
+        return {
           sectionStatus: status
         };
-        return newShareholderData;
       }
       case SECTIONS.REGISTER_LOCATIONS: {
-        const newRegisterLocationsData: RegisterLocationsData = {
+        return {
           sectionStatus: status
         };
-        return newRegisterLocationsData;
       }
   }
 };
