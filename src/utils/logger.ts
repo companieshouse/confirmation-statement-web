8a879a6d05bf2c9ff8450ea89d7b6298912fd790
svarallo <svarallo@companieshouse.gov.uk>
2021-11-05 11:22:39 +0000

    NCS-820 updating logger name to match others on kibana

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,7 +1,7 @@
 import { createLogger } from "@companieshouse/structured-logging-node";
 import ApplicationLogger from "@companieshouse/structured-logging-node/lib/ApplicationLogger";
 
-export const logger: ApplicationLogger = createLogger("Confirmation Statement Web");
+export const logger: ApplicationLogger = createLogger("confirmation-statement-web");
 
 export const createAndLogError = (description: string): Error => {
   const error = new Error (description);
