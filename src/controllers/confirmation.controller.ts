a3b470570df3eb11ed1963d06c481f67a48ec20f
ICoxCH <icox@companieshouse.gov.uk>
2021-11-25 12:17:37 +0000

    Add email to confirmation page and ammend wording

--- src/controllers/confirmation.controller.ts
+++ src/controllers/confirmation.controller.ts
@@ -1,12 +1,16 @@
 import { NextFunction, Request, Response } from "express";
 import { Templates } from "../types/template.paths";
 import { urlUtils } from "../utils/url";
+import {Session} from "@companieshouse/node-session-handler";
 
 export const get = (req: Request, res: Response, next: NextFunction) => {
   try {
     const transactionId = urlUtils.getTransactionIdFromRequestParams(req);
+    const session = req.session as Session;
+    const userEmail = session.data.signin_info?.user_profile?.email;
     return res.render(Templates.CONFIRMATION, {
       referenceNumber: transactionId,
+      userEmail
     });
   } catch (e) {
     return next(e);
