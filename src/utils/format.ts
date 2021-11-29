1c3649c43e0e2eea22323ce686c084467bfc202b
kheseyongya <kheseyongya@gmail.com>
2021-11-29 12:43:22 +0000

    Use enum

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,5 +1,6 @@
 import { RegisteredOfficeAddress } from "@companieshouse/api-sdk-node/dist/services/company-profile/types";
 import { ActiveOfficerDetails, Address, PersonOfSignificantControl } from "@companieshouse/api-sdk-node/dist/services/confirmation-statement";
+import { OFFICER_ROLE } from "./constants";
 
 export const formatTitleCase = (str: string|undefined): string =>  {
   if (!str) {
@@ -94,7 +95,7 @@ export const formatPSCForDisplay = (psc: PersonOfSignificantControl): PersonOfSi
 export const formatSecretaryList = (officers: ActiveOfficerDetails[]): any[] => {
   const secretaryList = new Array(0);
   for (const officer of officers) {
-    if (officer.role === "Secretary" && officer.isCorporate === false ) {
+    if (officer.role === OFFICER_ROLE.SECRETARY && officer.isCorporate === false ) {
       const serviceAddress = formatAddressForDisplay(formatAddress(officer.serviceAddress));
       const surname = officer.surname;
       const forename = formatTitleCase(officer.foreName1);
