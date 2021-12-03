6e9a4c0aab39fcec73ec56fb37bc53805a0dc707
svarallo <svarallo@companieshouse.gov.uk>
2021-12-03 14:55:44 +0000

    BI-9871 Changing officer details service to get officer details from public api

--- src/services/active.officers.details.service.ts
+++ src/services/active.officers.details.service.ts
@@ -1,42 +1,24 @@
 import { Resource } from "@companieshouse/api-sdk-node";
 import { ApiErrorResponse } from "@companieshouse/api-sdk-node/dist/services/resource";
-import { Session } from "@companieshouse/node-session-handler";
-import { createPublicOAuthApiClient } from "./api.service";
-import {
-  ActiveOfficerDetails,
-  ConfirmationStatementService
-} from "@companieshouse/api-sdk-node/dist/services/confirmation-statement";
-import { OFFICER_ROLE, OFFICER_TYPE } from "../utils/constants";
+import { createPublicApiKeyClient } from "./api.service";
+import { CompanyOfficer, CompanyOfficers } from "@companieshouse/api-sdk-node/dist/services/company-officers";
+import { createAndLogError } from "../utils/logger";
 
-export const getActiveOfficersDetailsData = async (session: Session, transactionId: string, submissionId: string): Promise<ActiveOfficerDetails[]> => {
-  const client = createPublicOAuthApiClient(session);
-  const csService: ConfirmationStatementService = client.confirmationStatementService;
-  const response: Resource<ActiveOfficerDetails[]> | ApiErrorResponse = await csService.getListActiveOfficerDetails(transactionId, submissionId);
-  const status = response.httpStatusCode as number;
-
+export const getActiveOfficersDetailsData = async (companyNumber: string): Promise<CompanyOfficer[]> => {
+  const client = createPublicApiKeyClient();
+  const companyOfficersDetailsResource: Resource<CompanyOfficers> = await client.companyOfficers.getCompanyOfficers(companyNumber, 5, 0, false, "resigned_on");
+  const companyOfficersDetails: CompanyOfficers = companyOfficersDetailsResource.resource as CompanyOfficers;
+  const status = companyOfficersDetailsResource.httpStatusCode as number;
   if (status >= 400) {
-    const errorResponse = response as ApiErrorResponse;
+    const errorResponse = companyOfficersDetailsResource as ApiErrorResponse;
     throw new Error(`Error retrieving active officer details: ${JSON.stringify(errorResponse)}`);
   }
-  const successfulResponse = response as Resource<ActiveOfficerDetails[]>;
-  return successfulResponse.resource as ActiveOfficerDetails[];
-};
-
-export const getOfficerTypeList = (officerList: ActiveOfficerDetails[]) => {
-  const officerTypeList = new Array(0);
-  for (const officer of officerList){
-    if (OFFICER_ROLE.SECRETARY.localeCompare(officer.role, 'en', { sensitivity: 'accent' }) === 0 && !officer.isCorporate){
-      officerTypeList.push(OFFICER_TYPE.NATURAL_SECRETARY);
-    }
-    if (OFFICER_ROLE.SECRETARY.localeCompare(officer.role, 'en', { sensitivity: 'accent' }) === 0 && officer.isCorporate){
-      officerTypeList.push(OFFICER_TYPE.CORPORATE_SECRETARIES);
-    }
-    if (OFFICER_ROLE.DIRECTOR.localeCompare(officer.role, 'en', { sensitivity: 'accent' }) === 0 && !officer.isCorporate){
-      officerTypeList.push(OFFICER_TYPE.NATURAL_DIRECTOR);
-    }
-    if (OFFICER_ROLE.DIRECTOR.localeCompare(officer.role, 'en', { sensitivity: 'accent' }) === 0 && officer.isCorporate){
-      officerTypeList.push(OFFICER_TYPE.CORPORATE_DIRECTORS);
-    }
+  if (parseInt(companyOfficersDetails.activeCount, 10) > 5) {
+    throw createAndLogError(`Active officer count for company ${companyNumber} is greater than 5 with value of ${companyOfficersDetails.activeCount}`);
   }
-  return officerTypeList;
+  if (parseInt(companyOfficersDetails.activeCount, 10) === 0) {
+    throw createAndLogError(`No active officers found for company ${companyNumber}, active count is 0`);
+  }
+
+  return companyOfficersDetails.items.filter(companyOfficer => !companyOfficer.resignedOn);
 };
