9b5e2c1e6c85c449e5ae555c39965fd97f752321
Matthew Culpin <mculpin@companieshouse.gov.uk>
2022-01-21 13:04:25 +0000

    BI-10288 Extract individual pscs and display

--- src/controllers/tasks/active.psc.details.controller.ts
+++ src/controllers/tasks/active.psc.details.controller.ts
@@ -2,14 +2,57 @@ import { NextFunction, Request, Response } from "express";
 import { TASK_LIST_PATH } from "../../types/page.urls";
 import { Templates } from "../../types/template.paths";
 import { urlUtils } from "../../utils/url";
+import { PersonOfSignificantControl } from "@companieshouse/api-sdk-node/dist/services/confirmation-statement";
+import { getPscs } from "../../services/psc.service";
+import { Session } from "@companieshouse/node-session-handler";
+import { appointmentTypes, LOCALE_EN } from "../../utils/constants";
+import { formatAddressForDisplay, formatPSCForDisplay } from "../../utils/format";
+import { toReadableFormat } from "../../utils/date";
 
-export const get = (req: Request, res: Response, next: NextFunction) => {
+
+
+export const get = async (req: Request, res: Response, next: NextFunction) => {
   try {
+    const transactionId = urlUtils.getTransactionIdFromRequestParams(req);
+    const submissionId = urlUtils.getSubmissionIdFromRequestParams(req);
+    const pscs: PersonOfSignificantControl[] = await getPscs(req.session as Session, transactionId, submissionId);
+
+    const pscLists = buildPscLists(pscs);
     return res.render(Templates.ACTIVE_PSC_DETAILS, {
       templateName: Templates.ACTIVE_PSC_DETAILS,
       backLinkUrl: urlUtils.getUrlToPath(TASK_LIST_PATH, req),
+      pscList: pscLists.individualPscList
     });
   } catch (e) {
     return next(e);
   }
+};
+
+const buildPscLists = (pscs: PersonOfSignificantControl[]): any => {
+  return {
+    individualPscList: buildIndividualPscList(pscs)
+  };
+};
+
+const buildIndividualPscList = (pscs: PersonOfSignificantControl[]): any[] => {
+  return pscs
+    .filter(psc => equalsIgnoreCase(psc.appointmentType, appointmentTypes.INDIVIDUAL_PSC))
+    .map(psc  => {
+      const formattedPsc: PersonOfSignificantControl = formatPSCForDisplay(psc);
+      const ura = formattedPsc.address ? formatAddressForDisplay(formattedPsc.address) : "";
+      const serviceAddress = formattedPsc.serviceAddress ? formatAddressForDisplay(formattedPsc.serviceAddress) : "";
+      const dob = psc.dateOfBirthIso ? toReadableFormat(psc.dateOfBirthIso) : "";
+      const dateOfAppointment = toReadableFormat(psc.appointmentDate);
+      return {
+        formattedPsc: formattedPsc,
+        ura: ura,
+        serviceAddress: serviceAddress,
+        dob: dob,
+        dateOfAppointment: dateOfAppointment
+      };
+    });
+};
+
+const equalsIgnoreCase = (pscType: string, wantedPscType: string): boolean => {
+  return wantedPscType.localeCompare(pscType, LOCALE_EN, { sensitivity: 'accent' }) === 0;
 };
--- test/controllers/tasks/active.psc.details.controller.unit.ts
+++ test/controllers/tasks/active.psc.details.controller.unit.ts
@@ -1,11 +1,18 @@
+import {mockPscList} from "../../mocks/active.psc.details.controller.mock";
+
+jest.mock("../../../src/services/psc.service");
+
 import mocks from "../../mocks/all.middleware.mock";
 import request from "supertest";
 import app from "../../../src/app";
 import { ACTIVE_PSC_DETAILS_PATH, urlParams } from "../../../src/types/page.urls";
+import { getPscs } from "../../../src/services/psc.service";
 
 const COMPANY_NUMBER = "12345678";
 const ACTIVE_PSC_DETAILS_URL = ACTIVE_PSC_DETAILS_PATH.replace(`:${urlParams.PARAM_COMPANY_NUMBER}`, COMPANY_NUMBER);
 const PAGE_HEADING = "Review the people with significant control";
+
+const mockGetPscs = getPscs as jest.Mock;
 
 describe("Active psc details controller tests", () => {
 
@@ -13,14 +20,25 @@ describe("Active psc details controller tests", () => {
     mocks.mockAuthenticationMiddleware.mockClear();
     mocks.mockServiceAvailabilityMiddleware.mockClear();
     mocks.mockSessionMiddleware.mockClear();
+    mockGetPscs.mockClear();
   });
 
   describe("get tests", () => {
 
-    it("Should navigate to active psc details page", async () => {
+    it("Should navigate to psc page and display individual psc details", async () => {
+      mockGetPscs.mockResolvedValueOnce(mockPscList);
       const response = await request(app).get(ACTIVE_PSC_DETAILS_URL);
-
       expect(response.text).toContain(PAGE_HEADING);
+      expect(response.text).toContain("Joe");
+      expect(response.text).toContain("Bloggs");
+      expect(response.text).toContain("British");
+      expect(response.text).toContain("21 March 1965");
+      expect(response.text).toContain("1 January 2012");
+      expect(response.text).toContain("10 This Road, This Town, Thisshire, Thisland, TH1 1AB");
+      expect(response.text).toContain("Diddly Squat Farm Shop, Chadlington, Thisshire, England, OX7 3PE");
+      expect(response.text).toContain("United Kingdom");
+      expect(response.text).toContain("75% or more of shares held as a person");
+      expect(response.text).toContain("Ownership of voting rights - more than 75%");
     });
   });
 });
