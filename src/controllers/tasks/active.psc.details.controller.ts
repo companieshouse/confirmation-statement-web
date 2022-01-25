a9d987fab041e16a41996c70e10850ccc940129e
svarallo <svarallo@companieshouse.gov.uk>
2022-01-25 14:51:33 +0000

    BI-10288 adding individual PSC page

--- src/controllers/tasks/active.psc.details.controller.ts
+++ src/controllers/tasks/active.psc.details.controller.ts
@@ -30,7 +30,7 @@ export const get = async (req: Request, res: Response, next: NextFunction) => {
     return res.render(Templates.ACTIVE_PSC_DETAILS, {
       templateName: Templates.ACTIVE_PSC_DETAILS,
       backLinkUrl: urlUtils.getUrlToPath(TASK_LIST_PATH, req),
-      pscList: pscLists.individualPscList,
+      individualPscList: pscLists.individualPscList,
       relevantLegalEntityList: pscLists.relevantLegalEntityList,
       otherRegistrablePersonList: pscLists.otherRegistrablePersonList
     });
