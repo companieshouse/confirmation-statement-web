1752ac5d24ff87b2e9f31c2c2bd86855b1134f7f
mwestacott <mwestacott2@companieshouse.gov.uk>
2022-02-18 14:19:55 +0000

    BI-10598 Implement validation of URL parameters
    * Transaction id and submission id validation added
    * New (common) validator and middleware
    * Unit testing of validator

---
+++ src/validators/url.id.validator.ts
@@ -0,0 +1,21 @@
+import { logger } from "../utils/logger";
+
+const MAX_LENGTH = 50;
+
+export const isUrlIdValid = (urlId: string): boolean => {
+
+  logger.debug("Check the URL id is valid");
+
+  if (!urlId) {
+    logger.error("No URL id supplied");
+    return false;
+  }
+
+  if (urlId.length > MAX_LENGTH) {
+    const truncatedUrlId: string = urlId.substring(0, MAX_LENGTH);
+    logger.error(`URL id exceeds ${MAX_LENGTH} characters - ${truncatedUrlId}...`);
+    return false;
+  }
+
+  return true;
+};
