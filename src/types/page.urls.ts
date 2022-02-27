9b2bda3cd936ebd3d24d0defab37b62aab7456ac
markpit <mpitten@companieshouse.gov.uk>
2022-02-27 19:39:49 +0000

    BI-10598 Removing railing slash from submission id path matcher

--- src/types/page.urls.ts
+++ src/types/page.urls.ts
@@ -17,7 +17,7 @@ export const COMPANY_AUTH_PROTECTED_BASE = `/company/:${urlParams.PARAM_COMPANY_
 export const ACTIVE_SUBMISSION_BASE = COMPANY_AUTH_PROTECTED_BASE +
   `transaction/:${urlParams.PARAM_TRANSACTION_ID}/submission/:${urlParams.PARAM_SUBMISSION_ID}/`;
 export const CONTAINS_TRANSACTION_ID = `/transaction/:${urlParams.PARAM_TRANSACTION_ID}`;
-export const CONTAINS_SUBMISSION_ID = `/submission/:${urlParams.PARAM_SUBMISSION_ID}/`;
+export const CONTAINS_SUBMISSION_ID = `/submission/:${urlParams.PARAM_SUBMISSION_ID}`;
 
 
 // Use _PATH consts for redirects
