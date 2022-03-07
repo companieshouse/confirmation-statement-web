8731e30fcfbd3d45e6163aee0b7b4b71009be6f8
mwestacott <mwestacott2@companieshouse.gov.uk>
2022-03-07 16:51:49 +0000

    BI-10602 Improvements to logging of validation failures
    * Log reason for failure in validators if multiple reasons are possible
    * Use same error logging approach (and return code) in the authentication middleware

--- src/validators/company.number.validator.ts
+++ src/validators/company.number.validator.ts
@@ -1,13 +1,24 @@
+import { logger } from "../utils/logger";
+
 const COMPANY_NUMBER_MATCHER: RegExp = /^(?:[a-z]|[a-z][a-z])?\d{6,8}?$/i;
 
 export const isCompanyNumberValid = (companyNumber: string): boolean => {
+  logger.debug("Checking company number is valid");
+
   if (!companyNumber) {
+    logger.error("No company number supplied");
     return false;
   }
 
   if (companyNumber.length !== 8) {
+    logger.error("Invalid company number length");
     return false;
   }
 
-  return COMPANY_NUMBER_MATCHER.test(companyNumber);
+  if (!COMPANY_NUMBER_MATCHER.test(companyNumber)) {
+    logger.error("Invalid company number format");
+    return false;
+  }
+
+  return true;
 };
