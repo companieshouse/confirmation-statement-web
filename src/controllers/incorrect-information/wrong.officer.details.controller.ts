941d6c5fca1fbd014bab8fbf985d4b2fb5b64bb7
rgoswami-ch <rgoswami@companieshouse.gov.uk>
2022-05-09 13:27:19 +0100

    Changed the wrong registers, wrong RO address, and wrong details (officers and psc) screen

--- src/controllers/incorrect-information/wrong.officer.details.controller.ts
+++ src/controllers/incorrect-information/wrong.officer.details.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { urlUtils } from "../../utils/url";
 import { ACTIVE_OFFICERS_DETAILS_PATH, TASK_LIST_PATH } from "../../types/page.urls";
 import { Templates } from "../../types/template.paths";
-import { WRONG_DETAILS_UPDATE_OFFICER, WRONG_DETAILS_UPDATE_OFFICERS } from "../../utils/constants";
+import { WRONG_DETAILS_UPDATE_OFFICER, WRONG_DETAILS_UPDATE_OFFICERS, DETAIL_TYPE_OFFICER } from "../../utils/constants";
 
 export const get = (req: Request, res: Response) => {
   return res.render(Templates.WRONG_DETAILS, {
@@ -10,6 +10,7 @@ export const get = (req: Request, res: Response) => {
     backLinkUrl: urlUtils.getUrlToPath(ACTIVE_OFFICERS_DETAILS_PATH, req),
     returnToTaskListUrl: urlUtils.getUrlToPath(TASK_LIST_PATH, req),
     stepOneHeading: WRONG_DETAILS_UPDATE_OFFICER,
+    detailType: DETAIL_TYPE_OFFICER,
     pageHeading: WRONG_DETAILS_UPDATE_OFFICERS,
   });
 };
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -18,6 +18,8 @@ export const WRONG_DETAILS_UPDATE_OFFICER = "Update the officer details";
 export const WRONG_DETAILS_UPDATE_OFFICERS = "Incorrect Officer Details";
 export const PSC_STATEMENT_NAME_PLACEHOLDER = "{linked_psc_name}";
 export const LOCALE_EN = "en";
+export const DETAIL_TYPE_OFFICER = "officer";
+export const DETAIL_TYPE_PSC = "PSC";
 
 export enum RADIO_BUTTON_VALUE {
   NO = "no",
