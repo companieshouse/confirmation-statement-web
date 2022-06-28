f5f5e7976d9c877365faede278143ae7dfb90207
Riddhiman Goswami <rgoswami@companieshouse.gov.uk>
2022-06-28 16:09:15 +0100

    Refactoring code to remove common web calls file.

--- src/controllers/incorrect-information/wrong.officer.details.controller.ts
+++ src/controllers/incorrect-information/wrong.officer.details.controller.ts
@@ -1,34 +1,44 @@
 import { NextFunction, Request, Response } from "express";
 import { urlUtils } from "../../utils/url";
-import { ACTIVE_OFFICERS_DETAILS_PATH } from "../../types/page.urls";
+import { ACTIVE_OFFICERS_DETAILS_PATH, TASK_LIST_PATH } from "../../types/page.urls";
 import { Templates } from "../../types/template.paths";
-import { WRONG_DETAILS_UPDATE_OFFICERS, DETAIL_TYPE_OFFICER, WRONG_OFFICER_ERROR, SECTIONS } from "../../utils/constants";
-import { getCommon, postCommon } from "../../utils/wrong.information.stop.screen.common.web.calls";
+import { WRONG_DETAILS_UPDATE_OFFICERS, DETAIL_TYPE_OFFICER, WRONG_OFFICER_ERROR, SECTIONS, RADIO_BUTTON_VALUE } from "../../utils/constants";
+import { SectionStatus } from "@companieshouse/api-sdk-node/dist/services/confirmation-statement";
+import { sendUpdate } from "../../utils/update.confirmation.statement.submission";
+import { getRadioButtonInvalidValueErrorMessage, isRadioButtonValueValid } from "../../validators/radio.button.validator";
 
 export const get = (req: Request, res: Response) => {
-  return { 
-    renderedPage: getCommon(req, res, Templates.WRONG_DETAILS, {
-      templateName: Templates.WRONG_DETAILS,
-      backLinkUrl: urlUtils.getUrlToPath(ACTIVE_OFFICERS_DETAILS_PATH, req),
-      detailType: DETAIL_TYPE_OFFICER,
-      detailTypeLegend: DETAIL_TYPE_OFFICER,
-      pageHeading: WRONG_DETAILS_UPDATE_OFFICERS,
-    }),
-  };
+  return res.render(Templates.WRONG_DETAILS, { 
+    templateName: Templates.WRONG_DETAILS,
+    backLinkUrl: urlUtils.getUrlToPath(ACTIVE_OFFICERS_DETAILS_PATH, req),
+    pageHeading: WRONG_DETAILS_UPDATE_OFFICERS,
+    detailType: DETAIL_TYPE_OFFICER,
+    detailTypeLegend: DETAIL_TYPE_OFFICER,
+  });
 };
 
 export const post = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    return {
-      renderedPage: postCommon(req, res, next, SECTIONS.ACTIVE_OFFICER, Templates.WRONG_DETAILS, {
-        templateName: Templates.WRONG_DETAILS,
-        backLinkUrl: urlUtils.getUrlToPath(ACTIVE_OFFICERS_DETAILS_PATH, req),
-        errorMsgText: WRONG_OFFICER_ERROR,
-        detailType: DETAIL_TYPE_OFFICER,
-        detailTypeLegend: DETAIL_TYPE_OFFICER,
-        pageHeading: WRONG_DETAILS_UPDATE_OFFICERS,
-      }),
-    };
+    const wrongOfficersRadioValue = req.body.radioButton;
+    if (!isRadioButtonValueValid(wrongOfficersRadioValue)) {
+      return next(new Error(getRadioButtonInvalidValueErrorMessage(wrongOfficersRadioValue)));
+    }
+    if (wrongOfficersRadioValue === RADIO_BUTTON_VALUE.YES) {
+      await sendUpdate(req, SECTIONS.ACTIVE_OFFICER, SectionStatus.RECENT_FILING);
+      return res.redirect(urlUtils.getUrlToPath(TASK_LIST_PATH, req));
+    }
+    if (wrongOfficersRadioValue === RADIO_BUTTON_VALUE.NO) {
+      await sendUpdate(req, SECTIONS.ACTIVE_OFFICER, SectionStatus.CONFIRMED);
+      return res.redirect(urlUtils.getUrlToPath(TASK_LIST_PATH, req));
+    }
+    return res.render(Templates.WRONG_DETAILS, {
+      templateName: Templates.WRONG_DETAILS,
+      backLinkUrl: urlUtils.getUrlToPath(ACTIVE_OFFICERS_DETAILS_PATH, req),
+      errorMsgText: WRONG_OFFICER_ERROR,
+      detailType: DETAIL_TYPE_OFFICER,
+      detailTypeLegend: DETAIL_TYPE_OFFICER,
+      pageHeading: WRONG_DETAILS_UPDATE_OFFICERS,
+    });
   } catch (e) {
     return next(e);
   }
--- src/controllers/incorrect-information/wrong.registered.office.address.controller.ts
+++ src/controllers/incorrect-information/wrong.registered.office.address.controller.ts
@@ -6,31 +6,41 @@ import {
   TASK_LIST_PATH
 } from "../../types/page.urls";
 import { Templates } from "../../types/template.paths";
-import { SECTIONS, WRONG_ROA_ERROR } from "../../utils/constants";
-import { getCommon, postCommon } from "../../utils/wrong.information.stop.screen.common.web.calls";
+import { RADIO_BUTTON_VALUE, SECTIONS, WRONG_ROA_ERROR } from "../../utils/constants";
+import { SectionStatus } from "@companieshouse/api-sdk-node/dist/services/confirmation-statement";
+import { sendUpdate } from "../../utils/update.confirmation.statement.submission";
+import { isRadioButtonValueValid, getRadioButtonInvalidValueErrorMessage } from "../../validators/radio.button.validator";
 
 export const get = (req: Request, res: Response) => {
-  return { 
-    renderedPage: getCommon(req, res, Templates.WRONG_RO, {
-      backLinkUrl: urlUtils.getUrlToPath(REGISTERED_OFFICE_ADDRESS_PATH, req),
-      taskListUrl: urlUtils.getUrlToPath(TASK_LIST_PATH, req),
-      changeRoaUrl: urlUtils.getUrlToPath(CHANGE_ROA_PATH, req),
-      templateName: Templates.WRONG_RO
-    }),
-  };
+  return res.render(Templates.WRONG_RO, {
+    backLinkUrl: urlUtils.getUrlToPath(REGISTERED_OFFICE_ADDRESS_PATH, req),
+    taskListUrl: urlUtils.getUrlToPath(TASK_LIST_PATH, req),
+    changeRoaUrl: urlUtils.getUrlToPath(CHANGE_ROA_PATH, req),
+    templateName: Templates.WRONG_RO
+  });
 };
 
 export const post = async (req: Request, res: Response, next: NextFunction) => {
-  
   try {
-    return {
-      renderedPage: postCommon(req, res, next, SECTIONS.ROA, Templates.WRONG_RO, {
-        backLinkUrl: urlUtils.getUrlToPath(REGISTERED_OFFICE_ADDRESS_PATH, req),
-        taskListUrl: urlUtils.getUrlToPath(TASK_LIST_PATH, req),
-        errorMsgText: WRONG_ROA_ERROR,
-        changeRoaUrl: urlUtils.getUrlToPath(CHANGE_ROA_PATH, req)
-      }),
-    };
+    const wrongRoaRadioValue = req.body.radioButton;
+    if (!isRadioButtonValueValid(wrongRoaRadioValue)) {
+      return next(new Error(getRadioButtonInvalidValueErrorMessage(wrongRoaRadioValue)));
+    }
+    if (wrongRoaRadioValue === RADIO_BUTTON_VALUE.YES) {
+      await sendUpdate(req, SECTIONS.ROA, SectionStatus.RECENT_FILING);
+      return res.redirect(urlUtils.getUrlToPath(TASK_LIST_PATH, req));
+    }
+    if (wrongRoaRadioValue === RADIO_BUTTON_VALUE.NO) {
+      await sendUpdate(req, SECTIONS.ROA, SectionStatus.CONFIRMED);
+      return res.redirect(urlUtils.getUrlToPath(TASK_LIST_PATH, req));
+    }
+    return res.render(Templates.WRONG_RO, {
+      backLinkUrl: urlUtils.getUrlToPath(REGISTERED_OFFICE_ADDRESS_PATH, req),
+      taskListUrl: urlUtils.getUrlToPath(TASK_LIST_PATH, req),
+      errorMsgText: WRONG_ROA_ERROR,
+      changeRoaUrl: urlUtils.getUrlToPath(CHANGE_ROA_PATH, req),
+      templateName: Templates.WRONG_RO
+    });
   } catch (e) {
     return next(e);
   }
