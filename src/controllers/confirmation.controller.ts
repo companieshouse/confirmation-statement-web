224f374e844a766f679384617e3d318a77695c25
Harry Bosworth <hbosworth@companieshouse.gov.uk>
2022-08-01 15:05:47 +0100

    Bypass signout confirmation screen

--- src/controllers/confirmation.controller.ts
+++ src/controllers/confirmation.controller.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { Templates } from "../types/template.paths";
 import { urlUtils } from "../utils/url";
 import { Session } from "@companieshouse/node-session-handler";
+import { ACCOUNTS_SIGNOUT_PATH } from "../types/page.urls";
 
 export const get = (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -9,6 +10,7 @@ export const get = (req: Request, res: Response, next: NextFunction) => {
     const session = req.session as Session;
     const userEmail = session.data.signin_info?.user_profile?.email;
     return res.render(Templates.CONFIRMATION, {
+      signoutURL: ACCOUNTS_SIGNOUT_PATH,
       referenceNumber: transactionId,
       userEmail
     });
