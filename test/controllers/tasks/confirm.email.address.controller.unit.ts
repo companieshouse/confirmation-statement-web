71c06ef26d3465fa3c9c103e64d8e8782a7614d3
CHssmith2 <ssmith2@companieshouse.gov.uk>
2023-11-17 10:34:22 +0000

    reinstate missing code due to being missed in conflict merge and additional test added

--- test/controllers/tasks/confirm.email.address.controller.unit.ts
+++ test/controllers/tasks/confirm.email.address.controller.unit.ts
@@ -1,20 +1,25 @@
-jest.mock("../../../src/middleware/company.authentication.middleware");
-
 import request from "supertest";
 import mocks from "../../mocks/all.middleware.mock";
 import { companyAuthenticationMiddleware } from "../../../src/middleware/company.authentication.middleware";
 import app from "../../../src/app";
-import { urlParams, CONFIRM_EMAIL_PATH } from "../../../src/types/page.urls";
+import { urlParams, TASK_LIST_PATH, CONFIRM_EMAIL_PATH } from "../../../src/types/page.urls";
 import { urlUtils } from "../../../src/utils/url";
+import { sendUpdate } from "../../../src/utils/update.confirmation.statement.submission";
+import { SECTIONS } from "../../../src/utils/constants";
+import { SectionStatus } from "@companieshouse/api-sdk-node/dist/services/confirmation-statement";
 
 const mockCompanyAuthenticationMiddleware = companyAuthenticationMiddleware as jest.Mock;
 mockCompanyAuthenticationMiddleware.mockImplementation((req, res, next) => next());
+const mockSendUpdate = sendUpdate as jest.Mock;
 
 const PAGE_HEADING = "Check the email address";
 const EXPECTED_ERROR_TEXT = "Sorry, the service is unavailable";
 const COMPANY_NUMBER = "12345678";
-
+const TASK_LIST_URL = TASK_LIST_PATH.replace(`:${urlParams.PARAM_COMPANY_NUMBER}`, COMPANY_NUMBER);
 const CONFIRM_EMAIL_ADDRESS_URL = CONFIRM_EMAIL_PATH.replace(`:${urlParams.PARAM_COMPANY_NUMBER}`, COMPANY_NUMBER);
+
+jest.mock("../../../src/middleware/company.authentication.middleware");
+jest.mock("../../../src/utils/update.confirmation.statement.submission");
 
 describe("Confirm Email Address controller tests", () => {
 
@@ -22,6 +27,7 @@ describe("Confirm Email Address controller tests", () => {
     mocks.mockAuthenticationMiddleware.mockClear();
     mocks.mockServiceAvailabilityMiddleware.mockClear();
     mocks.mockSessionMiddleware.mockClear();
+    
   });
 
   it("Should navigate to the Confirm Email Address page", async () => {
@@ -29,6 +35,15 @@ describe("Confirm Email Address controller tests", () => {
 
     expect(response.text).toContain(PAGE_HEADING);
     expect(response.text).toContain("Email address");
+  });
+
+  
+  it("Should return to task list page when email address is confirmed", async () => {
+    const response = await request(app).post(CONFIRM_EMAIL_ADDRESS_URL);
+    expect(mockSendUpdate.mock.calls[0][1]).toBe(SECTIONS.REA);
+    expect(mockSendUpdate.mock.calls[0][2]).toBe(SectionStatus.INITIAL_FILING);
+    expect(response.status).toEqual(302);
+    expect(response.header.location).toEqual(TASK_LIST_URL);
   });
 
   it("Should redirect to an error page when error is thrown", async () => {
