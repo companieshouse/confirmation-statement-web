e5eaf5e22957933f2d2d672f38318f3d52ac0342
tcbaileych <tbailey@companieshouse.gov.uk>
2024-06-18 17:04:23 +0100

    wip: added regex from rea web/api repository; adding additional checks for email supplied

--- src/validators/email.validator.ts
+++ src/validators/email.validator.ts
@@ -1,8 +1,41 @@
-const EMAIL_ADDRESS_MATCHER: RegExp = /^[\p{L}\p{N}\p{M}#+_-]+(\.[\p{L}\p{N}\p{M}#+_-]+)*@(?!-)[\p{L}\p{N}\p{M}#+_-]+(\.[\p{L}\p{N}\p{M}#+_-]+)*(\.[\p{L}\p{N}\p{M}#+_-]{2,})$/u;
+const VALID_EMAIL_REGEX_PATTERN = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~\\-]+@([^.@][^@\\s]+)$/u;
+const HOSTNAME_REGEX = /^(xn|[a-z0-9]+)(-?-[a-z0-9]+)*$/u;
+const TLD_PART_REGEX = /^(?:[a-z]{2,63}|xn--[a-z0-9]+(?:-[a-z0-9]+){1,4})(?:$|[^-])/u;
 
 export const isEmailAddressValid = (emailAddress: string): boolean => {
 
-  if (!EMAIL_ADDRESS_MATCHER.test(emailAddress)) {
+  if (!VALID_EMAIL_REGEX_PATTERN.test(emailAddress)) {
+    return false;
+  }
+
+  if (emailAddress.includes("..")) {
+    return false;
+  }
+
+  if (emailAddress.startsWith(".")) {
+    return false;
+  }
+
+  if (emailAddress.charAt(emailAddress.indexOf("@") - 1) == ".") {
+    return false;
+  }
+
+  const regexResult: RegExpMatchArray | null = emailAddress.match(VALID_EMAIL_REGEX_PATTERN);
+  if (!regexResult) {
+    return false;
+  }
+
+  const hostName = regexResult[1];
+  const parts = hostName.split(".");
+  if (parts.length < 2) {
+    return false;
+  }
+  for(const part of parts) {
+    if (!part.toLowerCase().match(HOSTNAME_REGEX)) {
+      return false;
+    }
+  }
+  if (!parts[parts.length - 1].toLowerCase().match(HOSTNAME_REGEX)) {
     return false;
   }
 
