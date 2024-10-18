cb6b98aecefc6b049e164c3d62133eeab7ef9cea
markpit <mpitten@companieshouse.gov.uk>
2024-10-18 13:44:32 +0100

    SEC-112 fix csrf test

--- test/controllers/error.controller.csrf.unit.ts
+++ test/controllers/error.controller.csrf.unit.ts
@@ -1,25 +1,12 @@
-jest.mock("@companieshouse/web-security-node");
+// jest.mock("@companieshouse/web-security-node");
 jest.mock("../../src/controllers/confirm.company.controller");
-jest.mock("../../src/validators/company.number.validator")
 
 import mocks from "../mocks/all.middleware.mock";
-import * as confirmCompanyController from "../../src/controllers/confirm.company.controller";
 import request from "supertest";
 import app from "../../src/app";
 import * as pageUrls from "../../src/types/page.urls";
 import { CsrfError } from "@companieshouse/web-security-node";
-import { authMiddleware } from "@companieshouse/web-security-node";
-import { isCompanyNumberValid } from "../../src/validators/company.number.validator";
-
-// get handle on mocked function and create mock function to be returned from calling companyAuthMiddleware
-const mockCompanyAuthMiddleware = authMiddleware as jest.Mock;
-const mockCompanyNumberValidator = isCompanyNumberValid as jest.Mock;
-mockCompanyNumberValidator.mockReturnValue(true);
-
-// when the mocked companyAuthMiddleware is called, make it return a mocked function so we can verify it gets called
-const mockAuthReturnedFunction = jest.fn();
-mockAuthReturnedFunction.mockImplementation((_req, _res, next) => next());
-mockCompanyAuthMiddleware.mockReturnValue(mockAuthReturnedFunction);
+import * as confirmCompanyController from "../../src/controllers/confirm.company.controller";
 
 const mockGetConfirmCompany = confirmCompanyController.get as jest.Mock;
 
@@ -31,6 +18,10 @@ describe("ERROR controller", () => {
 
   beforeEach(() => {
     jest.clearAllMocks();
+    // clearing the mocks will initialise them for first use as well as between tests
+    mocks.mockAuthenticationMiddleware.mockClear();
+    mocks.mockServiceAvailabilityMiddleware.mockClear();
+    mocks.mockSessionMiddleware.mockClear();
   });
 
   describe("CSRF error page tests", () => {
@@ -41,7 +32,6 @@ describe("ERROR controller", () => {
       expect(response.status).toEqual(403);
       expect(response.text).toContain(CSRF_ERROR_PAGE_HEADING);
       expect(response.text).toContain(CSRF_ERROR_PAGE_TEXT);
-      // expect(response.text).toContain(pageUrls.SERVICE_NAME);
-    });    
+    });
   });
 });
