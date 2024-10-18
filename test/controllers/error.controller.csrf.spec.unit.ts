a89e66553a90910e0ba7e9dd6a113f28554337b2
mattch1 <mculpin@companieshouse.gov.uk>
2024-10-18 13:40:16 +0100

    SEC-112 Chnages to match the mcocks in the company auth middleware test

--- test/controllers/error.controller.csrf.spec.unit.ts
+++ test/controllers/error.controller.csrf.spec.unit.ts
@@ -1,18 +1,27 @@
 jest.mock("@companieshouse/web-security-node");
+jest.mock("../../src/utils/logger");
 jest.mock("../../src/controllers/confirm.company.controller");
-jest.mock("../../src/validators/company.number.validator")
+jest.mock("../../src/validators/company.number.validator");
 
-import mocks from "../mocks/all.middleware.mock";
+import mockCsrfProtectionMiddleware from "../mocks/csrf.middleware.mock";
+import mockSessionMiddleware from "../mocks/session.middleware.mock";
+import mockServiceAvailabilityMiddleware from "../mocks/service.availability.middleware.mock";
+import mockAuthenticationMiddleware from "../mocks/authentication.middleware.mock";
+import mockIsPscQueryParameterValidationMiddleware from "../mocks/is.psc.validation.middleware.mock";
+import mockCompanyNumberQueryParameterValidationMiddleware from "../mocks/company.number.validation.middleware.mock";
+import mockTransactionIdValidationMiddleware from "../mocks/transaction.id.validation.middleware.mock";
+import mockSubmissionIdValidationMiddleware from "../mocks/submission.id.validation.middleware.mock";
 import * as confirmCompanyController from "../../src/controllers/confirm.company.controller";
 import request from "supertest";
 import app from "../../src/app";
 import * as pageUrls from "../../src/types/page.urls";
-import { CsrfError } from "@companieshouse/web-security-node";
-import { authMiddleware } from "@companieshouse/web-security-node";
+import { CsrfError, authMiddleware } from "@companieshouse/web-security-node";
 import { isCompanyNumberValid } from "../../src/validators/company.number.validator";
+import { logger } from "../../src/utils/logger";
 
 // get handle on mocked function and create mock function to be returned from calling companyAuthMiddleware
 const mockCompanyAuthMiddleware = authMiddleware as jest.Mock;
+const mockLoggerErrorRequest = logger.errorRequest as jest.Mock;
 const mockCompanyNumberValidator = isCompanyNumberValid as jest.Mock;
 mockCompanyNumberValidator.mockReturnValue(true);
 
@@ -23,14 +32,23 @@ mockCompanyAuthMiddleware.mockReturnValue(mockAuthReturnedFunction);
 
 const mockGetConfirmCompany = confirmCompanyController.get as jest.Mock;
 
-export const CSRF_TOKEN_ERROR = "CSRF token mismatch";
-export const CSRF_ERROR_PAGE_TEXT = "We have not been able to save the information you submitted on the previous screen.";
-export const CSRF_ERROR_PAGE_HEADING = "Sorry, something went wrong";
+const CSRF_TOKEN_ERROR = "CSRF token mismatch";
+const CSRF_ERROR_PAGE_TEXT = "We have not been able to save the information you submitted on the previous screen.";
+const CSRF_ERROR_PAGE_HEADING = "Sorry, something went wrong";
 
 describe("ERROR controller", () => {
 
   beforeEach(() => {
-    jest.clearAllMocks();
+    mockCompanyAuthMiddleware.mockClear();
+    mockSessionMiddleware.mockClear();
+    mockServiceAvailabilityMiddleware.mockClear();
+    mockAuthenticationMiddleware.mockClear();
+    mockIsPscQueryParameterValidationMiddleware.mockClear();
+    mockCompanyNumberQueryParameterValidationMiddleware.mockClear();
+    mockTransactionIdValidationMiddleware.mockClear();
+    mockSubmissionIdValidationMiddleware.mockClear();
+    mockLoggerErrorRequest.mockClear();
+    mockCsrfProtectionMiddleware.mockClear();
   });
 
   describe("CSRF error page tests", () => {
@@ -41,7 +59,6 @@ describe("ERROR controller", () => {
       expect(response.status).toEqual(403);
       expect(response.text).toContain(CSRF_ERROR_PAGE_HEADING);
       expect(response.text).toContain(CSRF_ERROR_PAGE_TEXT);
-      // expect(response.text).toContain(pageUrls.SERVICE_NAME);
     });    
   });
 });
