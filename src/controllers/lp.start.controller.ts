971bd635366a79a2222bd030ab753e0390d307c9
dho-ch <dho@companieshouse.gov.uk>
2025-06-06 11:20:13 +0100

    CSE-615 Initial routing to LP landing page

---
+++ src/controllers/lp.start.controller.ts
@@ -0,0 +1,6 @@
+import { Request, Response } from "express";
+import { Templates } from "../types/template.paths";
+
+export const get = (req: Request, res: Response) => {
+  return res.render(Templates.LP_START);
+};
