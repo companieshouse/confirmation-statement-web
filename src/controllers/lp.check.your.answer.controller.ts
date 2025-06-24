2fafc594a08d9afe793cc80da5e7b9e2899a96f2
dmcfaull-ch <dmcfaull@companieshouse.gov.uk>
2025-06-24 11:06:48 +0100

    Update lp.check.your.answer.controller.ts

--- src/controllers/lp.check.your.answer.controller.ts
+++ src/controllers/lp.check.your.answer.controller.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from "express";
 import { Templates } from "../types/template.paths";
 import { getLocaleInfo, getLocalesService, selectLang } from "../utils/localise";
+import { savePreviousPageInSession } from "../utils/session-navigation";
 import * as urls from "../types/page.urls";
 
 export const get = (req: Request, res: Response) => {
   const lang = selectLang(req.query.lang);
   const locales = getLocalesService();
-  const previousPage = urls.LP_CS_DATE_PATH;
+  const previousPage = savePreviousPageInSession(req);
   const nextPage = `${urls.LP_SIC_CODE_SUMMARY_PATH}?lang=${lang}`;
 
   return res.render(Templates.LP_CHECK_YOUR_ANSWER, {
