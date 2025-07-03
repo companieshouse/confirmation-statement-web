9b00fd551325b5885abd22db55b5d3c0ada44efd
sstevenson-ch <sstevenson@companieshouse.gov.uk>
2025-07-03 15:32:21 +0100

    Update page.urls.ts

--- src/types/page.urls.ts
+++ src/types/page.urls.ts
@@ -112,6 +112,6 @@ export const LP_CHECK_YOUR_ANSWER_PATH = CONFIRMATION_STATEMENT + LP_CHECK_YOUR_
 export const LP_SIC_CODE_SUMMARY = LIMITED_PARTNERSHIP + "/sic-code-summary"; 
 export const LP_SIC_CODE_SUMMARY_PATH = CONFIRMATION_STATEMENT + LP_SIC_CODE_SUMMARY; 
 export const LP_SIC_CODE_SUMMARY_ADD = LP_SIC_CODE_SUMMARY + "/add"; 
-export const LP_SIC_CODE_SUMMARY_REMOVE = LP_SIC_CODE_SUMMARY + "/:code/remove";  
+export const LP_SIC_CODE_SUMMARY_REMOVE = LP_SIC_CODE_SUMMARY + "/:code/remove";
 
 export const middlewarePaths = /\/confirmation-statement\/((?!healthcheck).)*/;
