a56f9478b5bc98c7cf7ffd1c354700458637725d
dmcfaull-ch <dmcfaull@companieshouse.gov.uk>
2025-07-08 15:53:35 +0100

    Revert "First"
    This reverts commit 604649fe1db10b37a9784c9ac5f2dfbac3473742.

--- src/controllers/confirmation.controller.ts
+++ src/controllers/confirmation.controller.ts
@@ -3,20 +3,16 @@ import { Templates } from "../types/template.paths";
 import { urlUtils } from "../utils/url";
 import { Session } from "@companieshouse/node-session-handler";
 import { ACCOUNTS_SIGNOUT_PATH } from "../types/page.urls";
-import { CompanyProfile } from "@companieshouse/api-sdk-node/dist/services/company-profile/types";
-import { validCompanyProfile } from "../../test/mocks/lp.company.profile.mock";
 
 export const get = (req: Request, res: Response, next: NextFunction) => {
   try {
     const transactionId = urlUtils.getTransactionIdFromRequestParams(req);
     const session = req.session as Session;
     const userEmail = session.data.signin_info?.user_profile?.email;
-    const company: CompanyProfile = validCompanyProfile;
     return res.render(Templates.CONFIRMATION, {
       signoutURL: ACCOUNTS_SIGNOUT_PATH,
       referenceNumber: transactionId,
-      userEmail, 
-      company
+      userEmail
     });
   } catch (e) {
     return next(e);
