2a5576e7656d148c9c457d1d53d0c008cea61162
sstevenson-ch <sstevenson@companieshouse.gov.uk>
2025-07-28 15:38:06 +0100

    removing commetned code

--- src/controllers/review.controller.ts
+++ src/controllers/review.controller.ts
@@ -34,8 +34,6 @@ export const get = async (req: Request, res: Response, next: NextFunction) => {
     const lang = selectLang(req.query.lang);
     res.cookie('lang', lang, { httpOnly: true });
 
-    // const company: CompanyProfile = validCompanyProfile;
-
     const company: CompanyProfile = await getCompanyProfile(companyNumber);
 
     if (company.type === "limited-partnership") {
