ba824bc0938f6dd05480c02861d2836238357bb3
dho-ch <dho@companieshouse.gov.uk>
2025-08-06 14:57:07 +0100

    CSE-800: ACSP path interceptor

--- test/mocks/all.middleware.mock.ts
+++ test/mocks/all.middleware.mock.ts
@@ -7,6 +7,7 @@ import mockTransactionIdValidationMiddleware from "./transaction.id.validation.m
 import mockIsPscQueryParameterValidationMiddleware from "./is.psc.validation.middleware.mock";
 import mockCompanyNumberQueryParameterValidationMiddleware from "./company.number.validation.middleware.mock";
 import mockCsrfMiddleware from "./csrf.middleware.mock";
+import mockAcspValidationMiddleware from "./acsp.validation.middleware.mock";
 
 export default {
   mockServiceAvailabilityMiddleware,
@@ -17,5 +18,6 @@ export default {
   mockTransactionIdValidationMiddleware,
   mockIsPscQueryParameterValidationMiddleware,
   mockCompanyNumberQueryParameterValidationMiddleware,
-  mockCsrfMiddleware
+  mockCsrfMiddleware,
+  mockAcspValidationMiddleware
 };
