906cfcb537066b68238ad07c201e13286c7bea42
dmcfaull-ch <dmcfaull@companieshouse.gov.uk>
2025-08-15 13:58:03 +0100

    Update review.controller.ts

--- src/controllers/review.controller.ts
+++ src/controllers/review.controller.ts
@@ -36,20 +36,7 @@ export const get = async (req: Request, res: Response, next: NextFunction) => {
     const confirmationDate = company.confirmationStatement?.nextMadeUpTo;
 
     if (isLimitedPartnershipCompanyType(company)) {
-      let backLinkPath = "";
-
-      const isDateChangedInSession = getAcspSessionData(session)?.changeConfirmationStatementDate;
-      const isPrivateFundLimitedPartnership = 
-        isPflpLimitedPartnershipCompanyType(company) ||
-        isSpflpLimitedPartnershipCompanyType(company);
-
-      if(!isPrivateFundLimitedPartnership){
-        backLinkPath = LP_SIC_CODE_SUMMARY_PATH;
-      }
-
-      backLinkPath = (isDateChangedInSession ?? false) ? LP_CHECK_YOUR_ANSWER_PATH : LP_CS_DATE_PATH;
-
-
+      const backLinkPath = getACSPBackPath(session, company);
       const previousPage = urlUtils.getUrlWithCompanyNumberTransactionIdAndSubmissionId(
         backLinkPath,
         companyNumber,
@@ -300,16 +287,17 @@ const isStatementCheckboxTicked = (checkboxValue: string): boolean => {
   return false;
 };
 
-// const getACSPBackPath = (session: Session, company: CompanyProfile) => {
-//   const isDateChangedInSession = getAcspSessionData(session)?.changeConfirmationStatementDate;
-//   const isPrivateFundLimitedPartnership = 
-//     isPflpLimitedPartnershipCompanyType(company) ||
-//     isSpflpLimitedPartnershipCompanyType(company);
+const getACSPBackPath = (session: Session, company: CompanyProfile) => {
+  const sessionData = getAcspSessionData(session);
+  const isDateChangedInSession = Boolean(sessionData?.changeConfirmationStatementDate);
+  const isPrivateFundLimitedPartnership = 
+    isPflpLimitedPartnershipCompanyType(company) ||
+    isSpflpLimitedPartnershipCompanyType(company);
 
-//   if(!isPrivateFundLimitedPartnership){
-//     return LP_SIC_CODE_SUMMARY_PATH;
-//   }
+  if(!isPrivateFundLimitedPartnership){
+    return LP_SIC_CODE_SUMMARY_PATH;
+  }
 
-//   return (isDateChangedInSession ?? false) ? LP_CHECK_YOUR_ANSWER_PATH : LP_CS_DATE_PATH;
+  return isDateChangedInSession ? LP_CHECK_YOUR_ANSWER_PATH : LP_CS_DATE_PATH;
 
-// };
+};
