01915acb7d933c3f54fb7859c0dba0622fe9e4da
dmcfaull-ch <dmcfaull@companieshouse.gov.uk>
2025-08-15 11:33:29 +0100

    Update review.controller.ts

--- src/controllers/review.controller.ts
+++ src/controllers/review.controller.ts
@@ -288,13 +288,13 @@ const isStatementCheckboxTicked = (checkboxValue: string): boolean => {
 };
 
 const getACSPBackPath = (session: Session, company: CompanyProfile) => {
-  // const isDateChangedInSession = getAcspSessionData(session)?.changeConfirmationStatementDate;
+  const isDateChangedInSession = getAcspSessionData(session)?.changeConfirmationStatementDate;
 
-  if (isPflpLimitedPartnershipCompanyType(company) || isSpflpLimitedPartnershipCompanyType(company)) {
-    return LP_CS_DATE_PATH;
-      // ? LP_CHECK_YOUR_ANSWER_PATH
-      // : LP_CS_DATE_PATH;
-  }
+  //if (isPflpLimitedPartnershipCompanyType(company) || isSpflpLimitedPartnershipCompanyType(company)) {
+    return isDateChangedInSession
+      ? LP_CHECK_YOUR_ANSWER_PATH
+      : LP_CS_DATE_PATH;
+  //}
 
-  return LP_SIC_CODE_SUMMARY_PATH;
+  //return LP_SIC_CODE_SUMMARY_PATH;
 };
