aea6db1a96eb1e73940de64d7af109ce88f6b423
kensayle-ch <ksayle@companieshouse.gov.uk>
2025-08-27 12:37:03 +0100

    CSE-819 - restored changes to test/controllers/review.controller.unit.ts and views/limited-partners/partials/lp-review.njk, now storing state of checkboxes as Strings not booleans

--- test/controllers/review.controller.unit.ts
+++ test/controllers/review.controller.unit.ts
@@ -11,7 +11,7 @@ import { Transaction } from "@companieshouse/api-sdk-node/dist/services/transact
 import request from "supertest";
 import mocks from "../mocks/all.middleware.mock";
 import app from "../../src/app";
-import { CONFIRMATION_PATH, LP_CHECK_YOUR_ANSWER_PATH, LP_CS_DATE_PATH, LP_SIC_CODE_SUMMARY_PATH, REVIEW_PATH, urlParams } from '../../src/types/page.urls';
+import { CONFIRMATION_PATH, REVIEW_PATH } from "../../src/types/page.urls";
 import { urlUtils } from "../../src/utils/url";
 import { validCompanyProfile } from "../mocks/company.profile.mock";
 import { getCompanyProfile } from "../../src/services/company.profile.service";
@@ -22,8 +22,8 @@ import { createAndLogError } from "../../src/utils/logger";
 import { dummyPayment, PAYMENT_JOURNEY_URL } from "../mocks/payment.mock";
 import { mockConfirmationStatementSubmission } from "../mocks/confirmation.statement.submission.mock";
 import { getConfirmationStatement, updateConfirmationStatement } from "../../src/services/confirmation.statement.service";
-import * as sessionAcspUtils from "../../src/utils/session.acsp";
-import * as limitedPartnershipUtils from "../../src/utils/limited.partnership";
+import { Request, Response, NextFunction } from "express";
+import { Session } from "@companieshouse/node-session-handler";
 
 const PropertiesMock = jest.requireMock('../../src/utils/properties');
 jest.mock('../../src/utils/properties', () => ({
@@ -52,7 +52,6 @@ const ERROR_PAGE_HEADING = "Service offline - File a confirmation statement";
 const COSTS_TEXT = "You will need to pay a fee of £34";
 const CONFIRMATION_STATEMENT_TEXT = "By continuing, you confirm that all information required to be delivered by the company pursuant to";
 const CONFIRMATION_STATEMENT_ECCT_TEXT = "confirm that all information required to be delivered by the company pursuant to";
-// const LAWFUL_ACTIVITY_STATEMENT_TEXT = "confirm that the intended future activities of the company are lawful";
 const CONFIRMATION_STATEMENT_ERROR = "Select if all required information is either delivered or being delivered for the confirmation statement date";
 const LAWFUL_ACTIVITY_STATEMENT_ERROR = "Select if intended future activities are lawful";
 const ERROR_HEADING = "There is a problem";
@@ -217,7 +216,6 @@ describe("review controller tests", () => {
         });
       expect(response.status).toBe(200);
       expect(response.text).toContain(CONFIRMATION_STATEMENT_ECCT_TEXT);
-      // expect(response.text).toContain(LAWFUL_ACTIVITY_STATEMENT_TEXT);
     });
 
     it("Should redirect to an error page when error is returned", async () => {
@@ -227,6 +225,71 @@ describe("review controller tests", () => {
       expect(response.text).toContain(SERVICE_UNAVAILABLE_TEXT);
     });
 
+    it("Should show review page for Limited Partnership with confirmation and lawful activity statement checkboxes checked", async () => {
+
+      setupLimitedPartnershipCompany();
+
+      setExtraDataOnSession(true, true);
+
+      const response = await request(app).get(URL);
+
+      expect(response.status).toBe(200);
+
+      expect(response.text).toMatch(/govuk-checkboxes__input.*confirmation-statement.*true/);
+      expect(response.text).toMatch(/govuk-checkboxes__input.*lawful-activity-statement.*true/);
+    });
+
+    it("Should show review page for Limited Partnership with confirmation and lawful activity statement checkboxes not checked", async () => {
+
+      setupLimitedPartnershipCompany();
+
+      setExtraDataOnSession(false, false);
+
+      const response = await request(app).get(URL);
+
+      expect(response.status).toBe(200);
+
+      expect(response.text).toMatch(/govuk-checkboxes__input.*confirmation-statement.*false/);
+      expect(response.text).toMatch(/govuk-checkboxes__input.*lawful-activity-statement.*false/);
+    });
+
+    it("Should show review page for Limited Partnership with confirmation and lawful activity statement checkboxe values not defined", async () => {
+
+      setupLimitedPartnershipCompany();
+
+      const response = await request(app).get(URL);
+
+      expect(response.status).toBe(200);
+
+      expect(response.text).toMatch(/govuk-checkboxes__input.*confirmation-statement.*false/);
+      expect(response.text).toMatch(/govuk-checkboxes__input.*lawful-activity-statement.*false/);
+    });
+
+    function setupLimitedPartnershipCompany() {
+      const mockLimitedPartnership = {
+        companyNumber: COMPANY_NUMBER,
+        type: "limited-partnership-lp",
+        companyName: "Test Company"
+      };
+
+      mockGetCompanyProfile.mockResolvedValueOnce(mockLimitedPartnership);
+    }
+
+    function setExtraDataOnSession(confirmationChecked: boolean, lawfulActivityChecked: boolean) {
+      const CONFIRMATION_STATEMENT_SESSION_KEY: string = 'CONFIRMATION_STATEMENT_CHECK_KEY';
+      const LAWFUL_ACTIVITY_STATEMENT_SESSION_KEY: string = 'LAWFUL_ACTIVITY_STATEMENT_CHECK_KEY';
+
+      mocks.mockSessionMiddleware.mockImplementation((req: Request, res: Response, next: NextFunction) => {
+
+        if (!req.session) {
+          req.session = new Session();
+        }
+        req.session.setExtraData(CONFIRMATION_STATEMENT_SESSION_KEY, confirmationChecked);
+        req.session.setExtraData(LAWFUL_ACTIVITY_STATEMENT_SESSION_KEY, lawfulActivityChecked);
+
+        return next();
+      });
+    }
   });
 
   describe("post tests", () => {
@@ -255,9 +318,7 @@ describe("review controller tests", () => {
         .post(URL).send({ confirmationStatement: "true", lawfulActivityStatement: "true" });
 
       expect(response.status).toBe(302);
-      // expect(response.text).toContain(SERVICE_UNAVAILABLE_TEXT);
     });
-
 
     it("Should reload the review page with error messages when both confirmation & lawful activity statement checkboxes not ticked", async () => {
       const mockLimitedPartnership = {
@@ -339,7 +400,6 @@ describe("review controller tests", () => {
       expect(response.header.location).toBe(PAYMENT_JOURNEY_URL);
     });
 
-
     it("Should update the lawful purpose statement", async () => {
       PropertiesMock.FEATURE_FLAG_ECCT_START_DATE_14082023 = "2023-01-01";
       const companyProfile = { companyType: "ltd", confirmationStatement: { nextMadeUpTo: "2024-01-01" } };
@@ -400,88 +460,5 @@ describe("review controller tests", () => {
       expect(response.status).toEqual(302);
       expect(response.header.location).toBe(PAYMENT_JOURNEY_URL);
     });
-  });
-
-  describe("Back link test", () => {
-    beforeEach(() => {
-      jest.clearAllMocks();
-      mockGetConfirmationStatement.mockReset();
-      mockGetConfirmationStatement.mockResolvedValue(mockConfirmationStatementSubmission);
-      jest.spyOn(limitedPartnershipUtils, "isACSPJourney").mockReturnValue(true);
-    });
-
-    it("should redirect to Check SIC Code page when back button clicked, IS a Limited Partnership and NOT a private fund type", async() => {
-      const mockLimitedPartnership = {
-        companyNumber: COMPANY_NUMBER,
-        type: "limited-partnership-lp",
-        companyName: "Test Company"
-      };
-      mockGetCompanyProfile.mockResolvedValueOnce(mockLimitedPartnership);
-
-      const response = await request(app)
-        .get(URL);
-
-      const backPath = LP_SIC_CODE_SUMMARY_PATH
-        .replace(`:${urlParams.PARAM_COMPANY_NUMBER}`, COMPANY_NUMBER)
-        .replace(`:${urlParams.PARAM_TRANSACTION_ID}`, TRANSACTION_ID)
-        .replace(`:${urlParams.PARAM_SUBMISSION_ID}`, SUBMISSION_ID);
-
-      expect(response.text).toContain(backPath);
-    });
-
-    it("should redirect to Date page when back button clicked, IS a private fund Limited Partnership and NO date change", async() => {
-      jest.spyOn(sessionAcspUtils, "getAcspSessionData").mockReturnValue({
-        changeConfirmationStatementDate: false,
-        beforeYouFileCheck: true,
-        newConfirmationDate: null,
-        confirmAllInformationCheck: false,
-        confirmLawfulActionsCheck: false
-      });
-
-      const mockLimitedPartnership = {
-        companyNumber: COMPANY_NUMBER,
-        type: "limited-partnership-pflp",
-        companyName: "Test Company"
-      };
-      mockGetCompanyProfile.mockResolvedValueOnce(mockLimitedPartnership);
-
-      const response = await request(app)
-        .get(URL);
-
-      const backPath = LP_CS_DATE_PATH
-        .replace(`:${urlParams.PARAM_COMPANY_NUMBER}`, COMPANY_NUMBER)
-        .replace(`:${urlParams.PARAM_TRANSACTION_ID}`, TRANSACTION_ID)
-        .replace(`:${urlParams.PARAM_SUBMISSION_ID}`, SUBMISSION_ID);
-
-      expect(response.text).toContain(backPath);
-    });
-
-    it("should redirect to Check Your Answer page when back button clicked, IS a private fund Limited Partnership and HAS a date change", async() => {
-      jest.spyOn(sessionAcspUtils, "getAcspSessionData").mockReturnValue({
-        changeConfirmationStatementDate: true,
-        beforeYouFileCheck: true,
-        newConfirmationDate: null,
-        confirmAllInformationCheck: false,
-        confirmLawfulActionsCheck: false
-      });
-
-      const mockLimitedPartnership = {
-        companyNumber: COMPANY_NUMBER,
-        type: "limited-partnership-pflp",
-        companyName: "Test Company"
-      };
-      mockGetCompanyProfile.mockResolvedValueOnce(mockLimitedPartnership);
-
-      const response = await request(app)
-        .get(URL);
-
-      const backPath = LP_CHECK_YOUR_ANSWER_PATH
-        .replace(`:${urlParams.PARAM_COMPANY_NUMBER}`, COMPANY_NUMBER)
-        .replace(`:${urlParams.PARAM_TRANSACTION_ID}`, TRANSACTION_ID)
-        .replace(`:${urlParams.PARAM_SUBMISSION_ID}`, SUBMISSION_ID);
-
-      expect(response.text).toContain(backPath);
-    });
-
   });
 });
