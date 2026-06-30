c4c5933bc60a33407e3ba2a50c42f591312143bb
dmcfaull-ch <dmcfaull@companieshouse.gov.uk>
2026-06-30 16:44:55 +0100

    Revert "UI Updates for invalid-company-status UI"
    This reverts commit 8733d535a2435692eaca4a5bb3085fbb5c201424.

--- src/types/template.paths.ts
+++ src/types/template.paths.ts
@@ -43,6 +43,6 @@ export enum Templates {
     LP_CS_DATE = "limited-partners/components/date/confirmation-statement-date.njk",
     LP_SIC_CODE_SUMMARY = "limited-partners/components/sic-code-summary/sic-code-summary.njk",
     LP_MUST_BE_AUTHORISED_AGENT = "limited-partners/components/must-be-authorised-agent/must-be-authorised-agent.njk",
-    LP_STOP_SCREEN = "limited-partners/components/stop-screen/stop-screen.njk",
+    LP_STOP_SCREEN = "limited-partners/components/stop-screen/stop-screen",
     LP_TRANSITIONAL_STOP_SCREEN = "limited-partners/components/stop-screen/transitional-stop-screen",
 }
