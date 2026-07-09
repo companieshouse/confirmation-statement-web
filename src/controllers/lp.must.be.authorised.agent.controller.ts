89ec2ced3ad50449525eb3abe1374076443526e9
sstevenson-ch <sstevenson@companieshouse.gov.uk>
2026-07-09 11:11:15 +0100

    fixing back button

--- src/controllers/lp.must.be.authorised.agent.controller.ts
+++ src/controllers/lp.must.be.authorised.agent.controller.ts
@@ -1,14 +1,19 @@
 import { Request, Response } from "express";
 import { Templates } from "../types/template.paths";
 import { addLangToUrl, getLocaleInfo, getLocalesService, selectLang } from "../utils/localise";
+import { urlUtils } from "../utils/url";
+import * as urls from "../types/page.urls";
 
 export const get = (req: Request, res: Response) => {
     const lang = selectLang(req.query.lang);
     const locales = getLocalesService();
 
+    let prevPageURL = `${urls.CONFIRM_COMPANY_PATH}?companyNumber=${urlUtils.getCompanyNumberFromRequestParams(req)}`;
+
     return res.render(Templates.LP_MUST_BE_AUTHORISED_AGENT, {
         ...getLocaleInfo(locales, lang),
         htmlLang: lang,
+        previousPageWithoutLang: prevPageURL,
         applyAsACSPLink: addLangToUrl("/register-as-companies-house-authorised-agent", lang),
     });
 };
