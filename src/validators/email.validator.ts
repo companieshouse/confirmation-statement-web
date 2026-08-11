a432384da029b0033416ec3e01370f065a9a7a29
sstevenson-ch <sstevenson@companieshouse.gov.uk>
2026-08-11 09:59:17 +0100

    fixing concourse failure

--- src/validators/email.validator.ts
+++ src/validators/email.validator.ts
@@ -5,7 +5,7 @@ export const isEmailAddressValid = (emailAddress: string): boolean => {
         return false;
     }
 
-    const regexResult: RegExpMatchArray | null = VALID_EMAIL_REGEX_PATTERN.exec(emailAddress);
+    const regexResult: RegExpMatchArray | null = emailAddress.match(VALID_EMAIL_REGEX_PATTERN);
     if (!regexResult) {
         return false;
     }
